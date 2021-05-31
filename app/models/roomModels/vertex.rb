d46bd0f3663c316d8802f3086a8680774042dfa1
Patric Edwards <u18009337@tuks.co.za>
2021-05-31 14:48:07 +0200

    Vertex class complete

--- app/models/roomModels/vertex.rb
+++ app/models/roomModels/vertex.rb
@@ -8,4 +8,46 @@ class Vertex
   int width
   int height
 
+  def setID(id)
+    this.id=id
+  end
+
+  def getID
+    return id
+  end
+
+  def setName(name)
+    this.name=name
+  end
+
+  def getName
+    return name
+  end
+
+  def setgraphicID(graphicID)
+    this.graphicID=graphicID
+  end
+
+  def getgraphicID
+    return graphicID
+  end
+
+  def setPosition(x,y)
+    this.x = x
+    this.y = y
+  end
+
+  def getPosition
+    return x,y
+  end
+
+  def setDimension(width,height)
+    this.width = width
+    this.height = height
+  end
+
+  def getDimensions
+    return width,height
+  end
+
 end
