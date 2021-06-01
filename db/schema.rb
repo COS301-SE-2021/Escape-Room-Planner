d0ab7e666d313910f944484a09e336629233db4e
Patric Edwards <u18009337@tuks.co.za>
2021-06-01 15:24:20 +0200

    Added fields to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_113120) do
+ActiveRecord::Schema.define(version: 2021_06_01_132244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 2021_06_01_113120) do
     t.string "graphicid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nextV"
+    t.time "estimatedTime"
+    t.string "description"
+    t.string "clue"
   end
 
 end
