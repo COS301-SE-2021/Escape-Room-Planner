38ff8f89fd88ee1a6143c6c551819e162b8d3621
PurryFury <egorkharatyan@gmail.com>
2021-06-02 16:33:05 +0200

    Initial template for vertex unit tests

--- test/models/vertex_test.rb
+++ test/models/vertex_test.rb
@@ -1,7 +1,33 @@
-require "test_helper"
+require 'test_helper'
 
 class VertexTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'cannot save a vertex of type Vertex' do
+    vertex = Vertex.new
+    assert_not vertex.save, 'Saved a vertex'
+  end
+
+  test 'cannot save vertex without escape room id'do
+    vertex = Vertex.new #need to make this of some type that is not Vertex
+    assert_not vertex.save, 'Saved vertex witohut foreign key'
+  end
+
+  test 'cannot save vertex without name' do
+    vertex = Vertex.new
+  end
+  
+  test 'cannot save vertex without pos x' do
+
+  end
+
+  test 'cannot save vertex without pos y' do
+
+  end
+
+  test 'cannot save vertex without width' do
+
+  end
+
+  test 'cannot save vertex without height' do
+
+  end
 end
