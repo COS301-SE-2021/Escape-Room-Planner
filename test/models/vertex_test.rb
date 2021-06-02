533180e907b77c7d93de30bc6b6eb28e889075e8
PurryFury <egorkharatyan@gmail.com>
2021-06-02 17:13:11 +0200

    More templates

--- test/models/vertex_test.rb
+++ test/models/vertex_test.rb
@@ -1,18 +1,19 @@
 require 'test_helper'
 
 class VertexTest < ActiveSupport::TestCase
-  test 'cannot save a vertex of type Vertex' do
+  test 'cannot store vertex without type' do
     vertex = Vertex.new
-    assert_not vertex.save, 'Saved a vertex'
+    assert_not vertex.save, 'Saved a vertex without a type'
   end
 
-  test 'cannot save vertex without escape room id'do
-    vertex = Vertex.new #need to make this of some type that is not Vertex
-    assert_not vertex.save, 'Saved vertex witohut foreign key'
+  test 'cannot save vertex without escape room id' do
+    vertex = Vertex.new # need to make this of some type that is not Vertex
+    assert_not vertex.save, 'Saved vertex without foreign key'
   end
 
   test 'cannot save vertex without name' do
     vertex = Vertex.new
+    assert_not vertex.save, 'Saved a vertex without a name'
   end
   
   test 'cannot save vertex without pos x' do
@@ -28,6 +29,26 @@ class VertexTest < ActiveSupport::TestCase
   end
 
   test 'cannot save vertex without height' do
+
+  end
+
+  test 'cannot save vertex without graphic id' do
+
+  end
+
+  test 'cannot save puzzle without estimated time' do
+
+  end
+
+  test 'cannot save puzzle without description' do
+
+  end
+
+  test 'cannot save clue without clue' do
+
+  end
+
+  test 'cannot save container' do
 
   end
 end
