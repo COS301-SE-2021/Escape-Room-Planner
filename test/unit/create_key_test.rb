0b5e62bc91015134d349287f5db6fa35488610e5
Kayla Latty <u17360812@tuks.co.za>
2021-06-02 16:51:39 +0200

    added create key test

---
+++ test/unit/create_key_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require_relative '../../app/Services/Room/CreateKeyRequest'
+require_relative '../../app/Services/Room/CreateKeyResponse'
+
+
+class CreateKeyTest < Minitest::Unit::TestCase
+
+  def test_CreateKeyResponse
+    key_test = CreateKeyResponse.new(1)
+    assert_equal(1, key_test.id, "Create Key Response has not set the id correctly")
+  end
+
+  #key doesn't have data to test
+  def test_CreateKeyRequest
+  end
+end
