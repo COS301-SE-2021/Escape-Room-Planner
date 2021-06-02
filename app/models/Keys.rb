bbe643f9dccd776a0fce155986932932b1439884
PurryFury <egorkharatyan@gmail.com>
2021-06-02 22:52:53 +0200

    Added some validations to subclasses of vertices

--- app/models/Keys.rb
+++ app/models/Keys.rb
@@ -1,3 +1,3 @@
 class Keys < Vertex
-
+  # no extra validations are needed here, it acts like a normal vertex
 end
--- app/models/vertex.rb
+++ app/models/vertex.rb
@@ -1,7 +1,7 @@
 class Vertex < ApplicationRecord
   belongs_to :escape_room
   validates :id, uniqueness: true # forces id to be unique no matter what, dont really know if we need this
-  validates :id, :name, :graphicid, presence: true # just presence for now
+  validates :type, :name, :graphicid, presence: true # just presence for now
   validates :posx, :posy, :width, :height, numericality: true # forces floats to these?
   # validates :nextV
   # validates :estimatedTime
