89ec03c5e64d87f4d1fd228b58da40e37485b4d2
Patric Edwards <u18009337@tuks.co.za>
2021-06-02 14:34:52 +0200

    Working on unit tests

--- test/unit/er_test.rb
+++ test/unit/er_test.rb
@@ -3,19 +3,21 @@ require 'minitest/autorun'
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
+require './app/Services/room_services'
+require './app/Services/create_escaperoom_request'
+require './app/Services/create_escaperoom_response'
 
 class ErTest < Minitest::Unit::TestCase
 
-  def setup
+    def test_world
 
-  end
 
-  def test
-    hello = 'world'
-    puts hello
-    assert_equal 'world', hello, "hello function should return 'world'"
-  end
+      assert_equal 'world', hello, "Hello.world should return a string called 'world'"
+    end
 
+    def test_flunk
+      flunk "You shall not pass"
+    end
 end
 
 
