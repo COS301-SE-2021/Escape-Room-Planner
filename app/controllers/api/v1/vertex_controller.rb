c22689b06f0d27397fbc1b0f50cc9b05a29cfb30
PurryFury <egorkharatyan@gmail.com>
2021-06-02 12:40:22 +0200

    Added Get param stuff for demonstration purposes, old one is commented

--- app/controllers/api/v1/vertex_controller.rb
+++ app/controllers/api/v1/vertex_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class VertexController < ApplicationController
       def index
-        vertices = Vertex.all
+        # vertices = Vertex.all()
+        vertices = Vertex.where(:escape_room_id => request.GET['roomId'])
         render json: {status: 'SUCCESS', message: 'Vertices', data: vertices}, status: :ok
       end
     end
