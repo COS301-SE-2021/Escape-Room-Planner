3c74bc0e2ac4fe5154bbf6ba6820d9ed5a7b4625
Donavan Roux <donavanroux2019@gmail.com>
2021-06-02 15:37:20 +0200

    updated createEscapeRoom

--- app/Services/room_services.rb
+++ app/Services/room_services.rb
@@ -2,6 +2,8 @@ class RoomServices
 
    def createEscapeRoom(request)
       @escapeRoom = EscapeRoom.new
+      @escapeRoomint = EscapeRoom.all.last.id
+      @escapeRoom.id = @escapeRoomint += 1
       @escapeRoom.save
       @response = CreateEscaperoomResponse.new(@escapeRoom.id)
       @response
