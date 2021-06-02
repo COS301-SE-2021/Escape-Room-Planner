520dfa3ac4bf72208d25ca274ce6ccec8d02fb80
Patric Edwards <u18009337@tuks.co.za>
2021-06-02 16:16:33 +0200

    Create escape room unit tests

--- app/Services/room_services.rb
+++ app/Services/room_services.rb
@@ -2,8 +2,6 @@ class RoomServices
 
    def createEscapeRoom(request)
       @escapeRoom = EscapeRoom.new
-      @escapeRoomint = EscapeRoom.all.last.id
-      @escapeRoom.id = @escapeRoomint += 1
       @escapeRoom.save
       @response = CreateEscaperoomResponse.new(@escapeRoom.id)
       @response
--- test/unit/er_test.rb
+++ test/unit/er_test.rb
@@ -3,16 +3,24 @@ require './app/Services/room_services'
 require './app/Services/create_escaperoom_request'
 require './app/Services/create_escaperoom_response'
 
+
+
 class ErTest < ActiveSupport::TestCase
+  def test_create
+    beforetest=EscapeRoom.count
 
-    def test_world
-      beforetest=EscapeRoom.count
+    req = CreateEscaperoomRequest.new
+    rs = RoomServices.new
+    rs.createEscapeRoom(req)
 
-      req= CreateEscaperoomRequest.new
-      res= null
-      rs=RoomServices.new
-      rs.createEscapeRoom(req)
+    assert_not_equal(EscapeRoom.count, beforetest)
+  end
 
-      assert_difference(EscapeRoom.count, beforetest)
-    end
+  def test_resp
+    req = CreateEscaperoomRequest.new
+    rs = RoomServices.new
+    resp = rs.createEscapeRoom(req)
+
+    assert_not_equal(resp.id, 0)
+  end
 end
