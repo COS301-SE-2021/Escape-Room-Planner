a0b3900398e97674b8f806875280003e2285a81a
Patric Edwards <u18009337@tuks.co.za>
2021-06-02 13:23:04 +0200

    Fixed API

--- app/controllers/api/v1/vertex_controller.rb
+++ app/controllers/api/v1/vertex_controller.rb
@@ -2,8 +2,7 @@ module Api
   module V1
     class VertexController < ApplicationController
       def index
-        # vertices = Vertex.all()
-        vertices = Vertex.where(:escape_room_id => request.GET['roomId'])
+        vertices = Vertex.all
         render json: {status: 'SUCCESS', message: 'Vertices', data: vertices}, status: :ok
       end
     end
