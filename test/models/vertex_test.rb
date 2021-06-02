fa6c23ef2adaf620004d67d3e4c10c1a26fb34d1
PurryFury <egorkharatyan@gmail.com>
2021-06-02 17:40:30 +0200

    Unit test and most templates. Escape room test file. Database has test environment db now.

--- test/models/vertex_test.rb
+++ test/models/vertex_test.rb
@@ -2,42 +2,32 @@ require 'test_helper'
 
 class VertexTest < ActiveSupport::TestCase
   test 'cannot store vertex without type' do
-    vertex = Vertex.new
+    vertex = Vertex.new # creates a vertex of type null
     assert_not vertex.save, 'Saved a vertex without a type'
   end
 
   test 'cannot save vertex without escape room id' do
-    vertex = Vertex.new # need to make this of some type that is not Vertex
-    assert_not vertex.save, 'Saved vertex without foreign key'
   end
 
   test 'cannot save vertex without name' do
-    vertex = Vertex.new
-    assert_not vertex.save, 'Saved a vertex without a name'
   end
-  
-  test 'cannot save vertex without pos x' do
 
+  test 'cannot save vertex without pos x' do
   end
 
   test 'cannot save vertex without pos y' do
-
   end
 
   test 'cannot save vertex without width' do
-
   end
 
   test 'cannot save vertex without height' do
-
   end
 
   test 'cannot save vertex without graphic id' do
-
   end
 
   test 'cannot save puzzle without estimated time' do
-
   end
 
   test 'cannot save puzzle without description' do
@@ -45,10 +35,8 @@ class VertexTest < ActiveSupport::TestCase
   end
 
   test 'cannot save clue without clue' do
-
   end
 
   test 'cannot save container' do
-
   end
 end
