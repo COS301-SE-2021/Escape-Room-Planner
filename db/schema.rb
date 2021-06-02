541af4c89e37ddbe12cc9d732d4e729812b1b456
Patric Edwards <u18009337@tuks.co.za>
2021-06-02 18:38:47 +0200

    Made develop stable- removed non working code

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_02_081240) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "escape_rooms", force: :cascade do |t|
+    t.string "startVertex"
+    t.string "endVertex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "endVertex"
-    t.string "startVertex"
   end
 
   create_table "vertices", force: :cascade do |t|
