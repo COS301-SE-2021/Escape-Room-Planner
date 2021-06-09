cc25db8505eee099eeca1c985de3c6ec315cfdc3
Patric Edwards <u18009337@tuks.co.za>
2021-06-09 13:11:50 +0200

    Small changes

--- app/controllers/api/v1/vertex_controller.rb
+++ app/controllers/api/v1/vertex_controller.rb
@@ -10,7 +10,6 @@ module Api
       def create
         begin
           type = params[:type]
-          puts type
 
           Puzzle.create if type == 'Puzzle'
 
@@ -20,9 +19,9 @@ module Api
 
           Clue.create if type == 'Clue'
 
-          render json: {status: 'SUCCESS', message: 'Vertex:', data: 'Created: '+type}, status: :ok
+          render json: {status: 'SUCCESS', message: 'Vertex:', data: "Created: #{type}"}, status: :ok
         rescue StandardError
-          render json: {status: 'Fail', message: 'Ensure type is correct'}, status: :not_found
+          render json: {status: 'Fail', message: 'Ensure type is correct with correct parameters'}, status: :not_found
         end
       end
     end
