51ff6e625dffa17334afecfbbc6273abeef43252
PurryFury <egorkharatyan@gmail.com>
2021-06-09 14:45:49 +0200

    Constructor for puzzle added, nextV removed from it

--- app/Services/create_puzzle_request.rb
+++ app/Services/create_puzzle_request.rb
@@ -1,18 +1,17 @@
 class CreatePuzzleRequest
 
-  attr_accessor :name, :posx, :posy, :width, :height, :graphicid, :nextV, :estimatedTime, :description, :roomID
+  attr_accessor :name, :posx, :posy, :width, :height, :graphicid, :estimatedTime, :description, :roomID
   
-  def initialize
-    @name = nil
-    @posx = nil
-    @posy = nil
-    @width = nil
-    @height = nil
-    @graphicid = nil
-    @nextV = nil
+  def initialize(name, posx, posy, width, height, graphicid, escape_room_id)
+    @name = name
+    @posx = posx
+    @posy = posy
+    @width = width
+    @height = height
+    @graphicid = graphicid
     @estimatedTime = nil
     @description = nil
-    @roomID = nil
+    @roomID = escape_room_id
   end
 
 end
