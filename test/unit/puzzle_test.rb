3be4af1ef8acd03e8f5c7b6356fc468132956660
PurryFury <egorkharatyan@gmail.com>
2021-06-09 15:00:15 +0200

    Fixed my Pepega fix, all works now

--- test/unit/puzzle_test.rb
+++ test/unit/puzzle_test.rb
@@ -7,42 +7,15 @@ class PuzzleTest < ActiveSupport::TestCase
     beforetest = Puzzle.count
     room = EscapeRoom.new
     room.save
-    roomID = room.id
-    req = CreatePuzzleRequest.new
-    req.roomID = roomID
-    req.name = 'test'
-    req.description = 'test'
-    req.estimatedTime = Time.now
-    req.graphicid = 'test'
-    req.height = 0
-    req.width = 0
-    req.posx = 0
-    req.posy = 0
+    room_id = room.id
+    req = CreatePuzzleRequest.new 'test', 0, 0, 0, 0,
+                                  'graphic', Time.now, 'test description', room_id
     rs = RoomServices.new
-    rs.createPuzzle(req)
+    response = rs.createPuzzle(req)
+
+    puts response.to_yaml
 
     assert_not_equal(Puzzle.count, beforetest)
-  end
-
-  def test_checkCreatePuzzleSaved
-
-    room = EscapeRoom.new
-    room.save
-    roomID = room.id
-    req = CreatePuzzleRequest.new
-    req.roomID = roomID
-    req.name = 'test'
-    req.description = 'test'
-    req.estimatedTime = Time.now
-    req.graphicid = 'test'
-    req.height = 0
-    req.width = 0
-    req.posx = 0
-    req.posy = 0
-    rs = RoomServices.new
-    resp = rs.createPuzzle(req)
-
-    assert_not_equal(resp.id, 0)
   end
 
   def test_checkCreatePuzzleNullRequest
