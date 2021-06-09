4c100ea9e29393f00c1e4ed4a07612676da41b3e
Patric Edwards <u18009337@tuks.co.za>
2021-06-09 13:01:35 +0200

    Working on Vertex Create

--- app/controllers/api/v1/vertex_controller.rb
+++ app/controllers/api/v1/vertex_controller.rb
@@ -1,9 +1,29 @@
 module Api
   module V1
     class VertexController < ApplicationController
+      protect_from_forgery with: :null_session
       def index
         vertices = Vertex.all
         render json: {status: 'SUCCESS', message: 'Vertices', data: vertices}, status: :ok
+      end
+
+      def create
+        begin
+          type = params[:type]
+          puts type
+
+          Puzzle.create if type == 'Puzzle'
+
+          Container.create if type == 'Container'
+
+          Keys.create if type == 'Key'
+
+          Clue.create if type == 'Clue'
+
+          render json: {status: 'SUCCESS', message: 'Vertex:', data: 'Created: '+type}, status: :ok
+        rescue StandardError
+          render json: {status: 'Fail', message: 'Ensure type is correct'}, status: :not_found
+        end
       end
     end
   end
