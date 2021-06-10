36cd74ed4a8255ce7406956378b8600496c287c2
PurryFury <egorkharatyan@gmail.com>
2021-06-10 16:32:02 +0200

    Extra negative unit tests for Vertex

--- app/models/vertex.rb
+++ app/models/vertex.rb
@@ -7,8 +7,7 @@ class Vertex < ApplicationRecord
   validates :id, uniqueness: true # forces id to be unique no matter what, dont really know if we need this
   validates :type, :name, :graphicid, presence: true # just presence for now
   validates :posx, :posy, numericality: true
-  validates :width, :height, numericality: { greater_than: 0.0 }
-  # validates :nextV
+  validates :width, :height, numericality: { greater_than: 0.0 } # forces to be a number and greater than 0
   # validates :estimatedTime
   # validates :description
   # validates :clue
