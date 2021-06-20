5971578af75dee37223859d531031834ceed6aa2
Donavan Roux <donavanroux2019@gmail.com>
2021-06-20 14:12:41 +0200

    added create vertex api call

--- Frontend/src/app/pages/room-creator/room-creator.component.ts
+++ Frontend/src/app/pages/room-creator/room-creator.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {BigInteger} from '@angular/compiler/src/i18n/big_integer';
 import {HttpClient} from '@angular/common/http';
+import {Time} from "@angular/common";
 
 @Component({
   selector: 'app-room-creator',
@@ -23,11 +24,11 @@ export class RoomCreatorComponent implements OnInit {
 
   //adds an object to drag on our 'canvas'
   addObjects(type: string, loc: string, pos: number): void{
+
+  //  if()
+
     this.lastPos += pos;
     // @ts-ignore
-    // this.escapeRoomDivRef?.nativeElement.innerHTML += "<img src='./assets/images/"+loc+"' style='width: 50px; height: 50px;' alt='NOT FOUND' appDraggable>";
-    // this.objects+= "<img src='./assets/images/"+loc+"' style='width: 20px; height: 20px;' alt='NOT FOUND' class='resize-drag' appDraggable>";
-    //"<img src='./assets/images/room1.png' style='width: 20px; height: 20px;' alt='NOT FOUND' class='img-thumbnail w-10' appDraggable>"
     let newImage = this.renderer.createElement("img"); // create image
     this.renderer.addClass(newImage, "resize-drag");
     this.renderer.setStyle(newImage,"width", "75px");
@@ -83,6 +84,24 @@ export class RoomCreatorComponent implements OnInit {
     // append to the dropdown menu
     this.renderer.appendChild(this.escapeRoomListRef?.nativeElement, newRoom);
   }
+
+  //creates Vertex of type with scale at position
+  createVertex(inType: string, inName: string, inGraphicID: string, inPosy: number, inPosx: number, inWidth: number, inHeight: number, inEstimated_time: Time, inDescription: string, inRoomid: number): void{
+
+    //set initial json array for body
+    let createVertexBody = {type: inType, name: inName, graphicid: inGraphicID, posy: inPosy, posx: inPosx, width: inWidth, height: inHeight,estimated_time: inEstimated_time, description: inDescription, roomid: inRoomid};
+
+    //make post request for new vertex
+    this.httpClient.post<any>("http://127.0.0.1:3000/api/v1/vertex/", createVertexBody).subscribe(
+      response => {
+        //will update vertex ID in html here
+        console.log(response);
+      },
+      error => console.error('There was an error retrieving your rooms', error)
+    );
+
+  }
+
 }
 
 interface EscapeRoomArray {
