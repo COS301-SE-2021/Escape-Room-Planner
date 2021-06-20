8f3b1e98e1e052e3efa9ca3b889d60f461d5765d
Egor Kharatyan <egorkharatyan@gmail.com>
2021-06-20 11:47:26 +0200

    Revert "Testing for all API calls"

--- app/controllers/api/v1/vertex_controller.rb
+++ app/controllers/api/v1/vertex_controller.rb
@@ -5,65 +5,39 @@ require './app/Services/create_container_request'
 require './app/Services/create_key_request'
 require './app/Services/create_key_response'
 require './app/Services/room_services'
-require './app/Services/remove_vertex_request'
-require './app/Services/remove_vertex_response'
-
-
-
 module Api
   module V1
     class VertexController < ApplicationController
       protect_from_forgery with: :null_session
-
-      def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-        headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      end
-
-      def cors_preflight_check
-        if request.method == :options
-          headers['Access-Control-Allow-Origin'] = '*'
-          headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-          headers['Access-Control-Allow-Headers'] = '*'
-          headers['Access-Control-Max-Age'] = '1728000'
-          render :text => '', :content_type => 'text/plain'
-        end
-      end
-
-
       def index
         vertices = Vertex.all
         render json: {status: 'SUCCESS', message: 'Vertices', data: vertices}, status: :ok
       end
 
       def create
-
+        
         type = params[:type]
 
-        name = params[:name]
+        name = params[:name] 
 
-        graphicid = params[:graphicid]
+        graphicid = params[:graphicid] 
 
-        posy = params[:posy]
+        posy = params[:posy] 
 
-        posx = params[:posx]
+        posx = params[:posx] 
 
-        width = params[:width]
+        width = params[:width] 
 
-        height = params[:height]
-
+        height = params[:height] 
+        
         estimated_time = params[:estimated_time]
-
+        
         description = params[:description]
-
-        clue= params[:clue]
 
         roomid= params[:roomid]
 
         if name.nil? || graphicid.nil? || posy.nil? || posx.nil? || width.nil? || height.nil? || roomid.nil?
-          render json: { status: 'FAILED', message: 'Ensure correct parameters are given' }, status: :bad_request
+          render json: { status: 'FAILED', message: 'Ensure correct parameters are given' }, status: :not_found
           return
         end
 
@@ -73,7 +47,7 @@ module Api
         when 'Puzzle'
 
           if estimated_time.nil? || description.nil?
-            render json: { status: 'FAILED', message: 'Puzzle needs name and estimated time' }, status: :bad_request
+            render json: { status: 'FAILED', message: 'Ensure correct parameters are given' }, status: :not_found
             return
           end
 
@@ -85,53 +59,19 @@ module Api
           res = serv.createKey(req)
 
         when 'Container'
-          req = CreateContainerRequest.new(posx,posy,width,height,graphicid,roomid,name)
+          req = CreateContainerRequest.new
           res = serv.createContainer(req)
 
-        when 'Clue'
-          if clue.nil?
-            ender json: { status: 'FAILED', message: 'Ensure correct parameters are given' }, status: :bad_request
-            return
-          end
-
-          req=CreateClueRequest.new(name,posx,posy,width,height,graphicid,clue,roomid)
-          res=serv.createClue(req)
-
         else
-          render json: {status: 'FAILED', message: 'Ensure type is correct with correct parameters'}, status: :bad_request
+          render json: {status: 'FAILED', message: 'Ensure type is correct with correct parameters'}, status: :not_found
           return
         end
 
         render json: {status: 'SUCCESS', message: 'Vertex:', data: "Created: #{res.success}"}, status: :ok
       rescue StandardError
-        render json: {status: 'FAILED', message: 'Unspecified error'}, status: :bad_request
-
-      end #end create
-
-      def destroy
-        id=params[:id]
-
-        if id.nil?
-          render json: { status: 'FAILED', message: 'Delete needs an id to be passed in' }, status: :bad_request
-          return
-        end
-
-        serv = RoomServices.new
-        req= RemoveVertexRequest.new(id)
-        resp= serv.remove_vertex(req)
-
-
-        unless resp.success
-          render json: {status: 'FAILED', message: 'Unspecified error'}, status: :bad_request
-          return
-        end
-
-        render json: {status: 'SUCCESS', message: 'Vertex:', data: "Deleted: #{id}"}, status: :ok
-        rescue StandardError
-          render json: {status: 'FAILED', message: 'Unspecified error'}, status: :bad_request
-
+        render json: {status: 'FAILED', message: 'Unspecified error'}, status: :not_found
+        
       end
-
     end
   end
 end
