dc2a2b508d188776738284e585858d1e2bf658f9
Kayla Latty <u17360812@tuks.co.za>
2021-06-28 15:46:45 +0200

    Added user services file and made changes to register user request and response objects

--- app/Services/register_user_request.rb
+++ app/Services/register_user_request.rb
@@ -1,4 +1,4 @@
-class CreateUserRequest
+class RegisterUserRequest
   attr_accessor :username, :password, :email, :name, :isAdmin
 
   def initialize(username, password, email, name, isAdmin)
--- app/Services/register_user_response.rb
+++ app/Services/register_user_response.rb
@@ -1,4 +1,4 @@
-class CreateUserResponse
+class RegisterUserResponse
 
   attr_accessor :success, :message
 
