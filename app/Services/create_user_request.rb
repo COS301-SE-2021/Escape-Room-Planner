8b183a0a34b2863f974058cce41e75e106e44748
Kayla Latty <u17360812@tuks.co.za>
2021-06-28 15:25:29 +0200

    Added create user request class

---
+++ app/Services/create_user_request.rb
@@ -0,0 +1,12 @@
+class CreateUserRequest
+  attr_accessor :username, :password, :email, :name, :isAdmin
+
+  def initialize(username, password, email, name, isAdmin)
+    @username = username
+    @password = password
+    @email = email
+    @name = name
+    @isAdmin = isAdmin
+  end
+end
+
