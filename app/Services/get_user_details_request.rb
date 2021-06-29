bb4b28ee79fb4b658a070d35dd509e8d2183aed3
NQ-edile <nqfreelance@gmail.com>
2021-06-29 20:06:01 +0200

    Implemented get_user_details_request.rb and get_user_details_response.rb

---
+++ app/Services/get_user_details_request.rb
@@ -0,0 +1,10 @@
+class GetUserDetailsRequest
+  attr_accessor :username, :password, :email
+
+  def initialize(username, password, email)
+    @username = username
+    @password = password
+    @email = email
+  end
+end
+
