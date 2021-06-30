c0b457b07f2ad7e0b5bd45d768a1f21fe3904be2
Egor <egorkharatyan@gmail.com>
2021-06-30 17:14:17 +0200

    Response/Request objects are changed; Escape room validates presence of name

--- Backend/app/Services/create_escaperoom_request.rb
+++ Backend/app/Services/create_escaperoom_request.rb
@@ -1,3 +1,8 @@
 class CreateEscaperoomRequest
-  #later will need a user id to create
+  attr_accessor :name
+
+  # later will need a user id to create
+  def initialize(name)
+    @name = name
+  end
 end
--- Backend/app/models/escape_room.rb
+++ Backend/app/models/escape_room.rb
@@ -1,3 +1,4 @@
 class EscapeRoom < ApplicationRecord
   has_many :vertices
+  validates :name, presence: true
 end
