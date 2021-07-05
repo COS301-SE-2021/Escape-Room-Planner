dc933056ff8c228cc21090b0122c9bfbf9bd4ebc
Donavan Roux <donavanroux2019@gmail.com>
2021-07-05 14:08:05 +0200

    created puzzle model in front end and its constructor

--- Frontend/src/app/models/clue.model.spec.ts
+++ Frontend/src/app/models/clue.model.spec.ts
@@ -2,6 +2,6 @@ import { Clue } from './clue.model';
 
 describe('Clue', () => {
   it('should create an instance', () => {
-    expect(new Clue()).toBeTruthy();
+    expect(new Clue(0, 0, "", 0, 0, 0, 0, "", 0, "")).toBeTruthy();
   });
 });
