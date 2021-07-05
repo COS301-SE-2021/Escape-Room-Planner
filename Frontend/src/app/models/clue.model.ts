ed0996213d138e84ee2548099810655afac7931c
Egor <egorkharatyan@gmail.com>
2021-07-05 11:48:52 +0200

    Service conversion

--- Frontend/src/app/models/clue.model.ts
+++ Frontend/src/app/models/clue.model.ts
@@ -1,19 +1,28 @@
 import {Vertex} from "./vertex.model";
 
 export class Clue extends Vertex{
-  private clue: string;
+  private _clue: string;
 
-  constructor(local_id: bigint,
-              id: bigint,
+  constructor(local_id: number,
+              id: number,
               name: string,
               pos_x: number,
               pos_y: number,
               width: number,
               height: number,
               graphic_id: string,
-              room_id: bigint,
+              room_id: number,
               clue:string) {
     super( local_id, id, name, 'Clue', pos_x, pos_y, width, height, graphic_id, room_id );
-    this.clue = clue;
+    this._clue = clue;
+  }
+
+
+  get clue(): string {
+    return this._clue;
+  }
+
+  set clue(value: string) {
+    this._clue = value;
   }
 }
--- Frontend/src/app/services/vertex.service.ts
+++ Frontend/src/app/services/vertex.service.ts
@@ -5,9 +5,47 @@ import {Vertex} from "../models/vertex.model";
   providedIn: 'root'
 })
 export class VertexService {
-  private vertices : Vertex[]
+  private _local_id_count: number;
+  private _vertices : Vertex[];
 
   constructor() {
-    this.vertices = [];  //todo change this later
+    this._vertices = [];  //todo change this later
+    this._local_id_count = 0;
+  }
+
+  addVertex(inType: string, inName: string, inGraphicID: string,
+            inPos_y: number, inPos_x: number, inWidth: number,
+            inHeight: number, inEstimated_time: Date, inDescription: string,
+            inRoom_id: number, inClue: string): Vertex {
+
+    let createVertexBody = {type: inType,
+      name: inName,
+      graphicid: inGraphicID,
+      posy: inPos_y,
+      posx: inPos_x,
+      width: inWidth,
+      height: inHeight,
+      estimated_time: inEstimated_time,
+      description: inDescription,
+      roomid: inRoom_id,
+      clue: inClue
+    };
+
+    if(inType != "Puzzle"){
+      // @ts-ignore
+      delete  createVertexBody.description;
+      // @ts-ignore
+      delete  createVertexBody.estimated_time;
+    }
+    // removes parameters for clue
+    if (inType != 'Clue'){
+      // @ts-ignore
+      delete createVertexBody.clue;
+    }
+
+  }
+
+  get vertices(): Vertex[] {
+    return this._vertices;
   }
 }
