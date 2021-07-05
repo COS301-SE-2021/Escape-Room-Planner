cdcea32d050ead4e5d01f13dc638dff8e7c15a3c
Egor <egorkharatyan@gmail.com>
2021-07-05 12:05:20 +0200

    addVertex created

--- Frontend/src/app/services/vertex.service.ts
+++ Frontend/src/app/services/vertex.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Vertex} from "../models/vertex.model";
+import {HttpClient} from "@angular/common/http";
+import {Clue} from "../models/clue.model";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ export class VertexService {
   private _local_id_count: number;
   private _vertices : Vertex[];
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this._vertices = [];  //todo change this later
     this._local_id_count = 0;
   }
@@ -16,7 +18,7 @@ export class VertexService {
   addVertex(inType: string, inName: string, inGraphicID: string,
             inPos_y: number, inPos_x: number, inWidth: number,
             inHeight: number, inEstimated_time: Date, inDescription: string,
-            inRoom_id: number, inClue: string): Vertex {
+            inRoom_id: number, inClue: string): Vertex|null{
 
     let createVertexBody = {type: inType,
       name: inName,
@@ -43,6 +45,21 @@ export class VertexService {
       delete createVertexBody.clue;
     }
 
+    //make post request for new vertex
+    this.httpClient.post<any>("http://127.0.0.1:3000/api/v1/vertex/", createVertexBody).subscribe(
+      response => {
+        // todo Fix this to instantiate proper class
+        let new_vertex = new Vertex(this._local_id_count++, response.data.id, inName,
+                                    inType, inPos_x, inPos_y, inWidth, inHeight, inGraphicID, inRoom_id);
+        return new_vertex;
+      },
+      error => {
+        console.error('There was an error while creating a vertex', error);
+        return null;
+      }
+    );
+
+    return null;
   }
 
   get vertices(): Vertex[] {
