9d2e1cc1d1f5b8a54bf358ede5896e81c0bc0e04
Kayla Latty <u17360812@tuks.co.za>
2021-07-05 22:34:18 +0200

    Added create user to register user

--- Backend/app/Services/user_services.rb
+++ Backend/app/Services/user_services.rb
@@ -1,6 +1,23 @@
 class UserServices
   def registerUser(request)
+    return RegisterUserResponse(false,"The register user request is null") if request == nil
+    # do i need to check that a user with that username doesn't exist
 
+
+    @newUser = User.new
+    @newUser.username = request.username
+    @newUser.email = request.email
+    # has_secure_password encrypts the password when passing it to the database. Need to double check with db migrate
+    @newUser.password_digest = request.password_digest
+    @newUser.name = request.name
+    @newUser.isAdmin = request.isAdmin
+
+    @response = if @newUser.save
+                  RegisterUserResponse(true, "New User Succesfully Registered")
+                else
+                  #How do we identify later what caused the database error
+                  RegisterUserResponse(false, "New User Failed to be registered")
+                end
   end
 
   def verifyAccount(request)
