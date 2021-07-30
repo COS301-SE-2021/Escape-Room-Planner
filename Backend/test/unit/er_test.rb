8d405d7944aeb5f4c0f3abcd0128f089ab60ec6f
Donavan Roux <donavanroux2019@gmail.com>
2021-07-30 13:13:04 +0200

    fix maybe 3

--- Backend/test/unit/er_test.rb
+++ Backend/test/unit/er_test.rb
@@ -2,8 +2,8 @@
 
 require 'test_helper'
 require './app/Services/room_services'
-require './app/Services/create_escapeRoom_request'
-require './app/Services/create_escapeRoom_response'
+require './app/Services/create_escaperoom_request'
+require './app/Services/create_escaperoom_response'
 
 # rubocop:disable Metrics/ClassLength
 class ErTest < ActiveSupport::TestCase
