eef784a14020d2c970120c91d4825b9c25f16a51
Egor <egorkharatyan@gmail.com>
2021-08-02 12:42:32 +0200

    Fixed bug of outer dropp out not being persistent

--- Backend/test/unit/er_test.rb
+++ Backend/test/unit/er_test.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 require './app/Services/room_services'
 require './app/Services/create_escaperoom_request'
 require './app/Services/create_escaperoom_response'
+require './app/models/Keys'
+require './app/models/Container'
 
 # rubocop:disable Metrics/ClassLength
 class ErTest < ActiveSupport::TestCase
