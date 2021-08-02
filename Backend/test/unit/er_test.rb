026b2d24311dedf463149bf1adcb0203fe4535ef
Egor <egorkharatyan@gmail.com>
2021-08-02 12:27:12 +0200

    Test for GH Actions

--- Backend/test/unit/er_test.rb
+++ Backend/test/unit/er_test.rb
@@ -4,6 +4,7 @@ require 'test_helper'
 require './app/Services/room_services'
 require './app/Services/create_escaperoom_request'
 require './app/Services/create_escaperoom_response'
+require './app/models/Keys'
 
 # rubocop:disable Metrics/ClassLength
 class ErTest < ActiveSupport::TestCase
