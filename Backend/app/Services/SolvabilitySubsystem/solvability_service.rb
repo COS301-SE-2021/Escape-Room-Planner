eaafcd980fdd041a8019939ac670e9c0f2a889c3
Patric Edwards <u18009337@tuks.co.za>
2021-08-04 10:28:37 +0200

    More error handling

--- Backend/app/Services/SolvabilitySubsystem/solvability_service.rb
+++ Backend/app/Services/SolvabilitySubsystem/solvability_service.rb
@@ -4,6 +4,11 @@ class SolvabilityService
 
     raise 'Solvability Request cant be null' if request.nil?
 
+    if request.startVert.nil? || request.endVert.nil || request.vertices.nil?
+      raise 'Parameters in request object cannot be null'
+    end
+
+    CalculateSolvableResponse.new(solvabilityHelper(request.startVert, request.endVert, request.vertices))
   end
 
   def calculateSetUpOrder(request)
@@ -17,4 +22,9 @@ class SolvabilityService
     raise 'Solvability Request cant be null' if request.nil?
 
   end
+  
+  def solvabilityHelper(startVert,endVert,vertices)
+    @response = false
+  end
+  
 end
