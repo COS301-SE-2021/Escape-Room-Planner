b6d6e9d5067a3aa989d71bc940c3d99afbc5ea07
Kayla Latty <u17360812@tuks.co.za>
2021-08-04 11:02:36 +0200

    Fixed unit test declarations

--- Backend/test/unit/user_test.rb
+++ Backend/test/unit/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require './app/Services/user_services'
 class UserTest < ActiveSupport::TestCase
 
-  def test_registerUser
+  test 'test register user' do
     before_test = User.count
     # req = RegisterUserRequest.new('rTest', 'rTest', 'rTest@gmail.com', false)
     req = RegisterUserRequest.new('TestUser', '1234Pass', 'test@gmail.com', false)
@@ -12,7 +12,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not_equal(User.count, before_test)
   end
 
-  def test_UserSave
+  test 'test a user saves' do
     req = RegisterUserRequest.new('TestUser', '1234Pass', 'test@gmail.com', false)
     us = UserServices.new
     resp = us.registerUser(req)
@@ -20,7 +20,7 @@ class UserTest < ActiveSupport::TestCase
     assert(resp.success)
   end
 
-  def test_registerUserNullRequest
+  test 'test register user with a null request' do
     req = nil
     us = UserServices.new
     resp = us.registerUser(req)
@@ -28,7 +28,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal(false, resp.success)
   end
 
-  def test_LoginWithCorrectCredentials
+  test 'test login with a valid user' do
     req = LoginRequest.new('testUser', 'testPass')
     us = UserServices.new
     resp = us.login(req)
@@ -36,7 +36,7 @@ class UserTest < ActiveSupport::TestCase
     assert(resp.success)
   end
 
-  def test_LoginWithIncorrectUsername
+  test 'test login with an invalid username' do
     req = LoginRequest.new('test', 'testPass')
     us = UserServices.new
     resp = us.login(req)
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal(false, resp.success)
   end
 
-  def test_LoginWithIncorrectPassword
+  test 'test login with an invalid password' do
     req = LoginRequest.new('test', 'rando')
     us = UserServices.new
     resp = us.login(req)
