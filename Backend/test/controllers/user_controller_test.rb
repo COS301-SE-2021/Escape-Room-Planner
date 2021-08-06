e6fecddb1328cc01e83359a190aba544f8ce4489
Kayla Latty <u17360812@tuks.co.za>
2021-08-06 13:16:40 +0200

    Fixed rubocop offenses in user controller test

--- Backend/test/controllers/user_controller_test.rb
+++ Backend/test/controllers/user_controller_test.rb
@@ -1,24 +1,18 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 require 'concurrent'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
   test 'can login with valid credentials' do
-    req = RegisterUserRequest.new('test1','1235','test@',false)
-    us = UserServices.new
-    us.register_user(req)
-    # BCrypt::Password.create
-    puts User.find_by_id(2)[:password_digest]
-    puts User.find_by_id(1)[:password_digest]
-    post "#{api_v1_user_index_path}", params: {
+    post api_v1_user_index_path.to_s, params: {
       type: 'login',
-      username: 'test1',
-      password: '1235'
+      username: 'testUser',
+      password: 'testPass'
     }, as: :as_json
-
 
     response = JSON.parse(@response.body)
     assert_response :ok
     assert_equal 'Login Successful', response['message']
-    puts response['auth_token']
   end
 end
