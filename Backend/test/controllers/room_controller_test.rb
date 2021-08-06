6a5a32a5928767dbba9bb89ec651b84f33090d79
Donavan Roux <donavanroux2019@gmail.com>
2021-08-06 11:14:29 +0200

    added controller to delete room and updated  models and tests

--- Backend/test/controllers/room_controller_test.rb
+++ Backend/test/controllers/room_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 require 'concurrent'
 
 class RoomControllerTest < ActionDispatch::IntegrationTest
-
-    #Get Rooms
+  # Get Rooms
   test 'can get all rooms' do
     get api_v1_room_index_path
 
@@ -12,22 +11,37 @@ class RoomControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-    #Get Room
+  # Get Room
   test 'can get certain index' do
     get "#{api_v1_room_index_path}/1"
     assert_response :success
   end
 
-    #Get Room Fails
+  # Get Room Fails
   test 'can get unmade index' do
     get "#{api_v1_room_index_path}/100"
     assert_response :not_found
   end
 
-    #Create Room
+  # Create Room
   test 'can create room' do
     post api_v1_room_index_path
     assert_response :success
   end
 
+  test 'can delete valid room' do
+    delete "#{api_v1_room_index_path}/1"
+
+    response = JSON.parse(@response.body)
+    assert_response :ok
+    assert_equal 'Deleted Room', response['message']
+  end
+
+  test 'can handle delete escape room when room not exist' do
+    delete "#{api_v1_room_index_path}/-1"
+
+    response = JSON.parse(@response.body)
+    assert_response :bad_request
+    assert_equal 'Room does not exist', response['message']
+  end
 end
