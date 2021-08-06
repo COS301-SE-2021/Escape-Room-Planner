aea2a289b228814329f03a3f6a8b674b8b997192
Kayla Latty <u17360812@tuks.co.za>
2021-08-06 09:15:01 +0200

    User controller test refactor for registerUser

--- Backend/test/controllers/user_controller_test.rb
+++ Backend/test/controllers/user_controller_test.rb
@@ -5,7 +5,7 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   test 'can login with valid credentials' do
     req = RegisterUserRequest.new('test1','1235','test@',false)
     us = UserServices.new
-    us.registerUser(req)
+    us.register_user(req)
     # BCrypt::Password.create
     puts User.find_by_id(2)[:password_digest]
     puts User.find_by_id(1)[:password_digest]
