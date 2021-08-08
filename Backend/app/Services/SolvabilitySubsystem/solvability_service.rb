890d2ac7e57bf108356fc14938e9732742385912
Patric Edwards <u18009337@tuks.co.za>
2021-08-08 18:01:47 +0200

    Solvability changes

--- Backend/app/Services/SolvabilitySubsystem/solvability_service.rb
+++ Backend/app/Services/SolvabilitySubsystem/solvability_service.rb
@@ -8,7 +8,7 @@ class SolvabilityService
       raise 'Parameters in request object cannot be null'
     end
 
-    CalculateSolvableResponse.new(solvabilityHelper(request.startVert, request.endVert, request.vertices))
+    CalculateSolvableResponse.new(detect_cycle(request))
   end
 
   def calculate_set_up_order(request)
@@ -23,33 +23,41 @@ class SolvabilityService
 
   end
 
-  def solvability_helper(startVert,endVert,vertices)
-    @response = false
-  end
+  # Create the graph using the given number of edges and vertices.
+  # Create a recursive function that initializes the current index or vertex, visited, and recursion stack.
+  # Mark the current node as visited and also mark the index in recursion stack.
+  # Find all the vertices which are not visited and are adjacent to the current node. Recursively call the function for those vertices, If the recursive function returns true, return true.
+  # If the adjacent vertices are already marked in the recursion stack then return true.
+  # Create a wrapper class, that calls the recursive function for all the vertices and if any function returns true return true. Else if for all vertices the function returns false return false.
 
-  def detect_cycle()
+  def detect_cycle(request)
 
-    #Two arrays of size of num vertices set to false
-    visited = Array.new
-    restack = Array.new
-
+    # Two arrays of size of num vertices set to false
+    visited = Array.new(request.vertices.count)
     i = 0
     while i < visited.count
-
+      visited[i] = false
     end
+    
+    stack = []
 
+    i = 0
+
+    false
   end
 
-  def is_cyclic(i,visited,restack)
-    if restack[i]
-      return true
-    end
+  def cyclic(index, visited, stack)
+    # if node has been stack or visited return true
+    return true if stack[index]
 
-    if visited[i]
-      return true
-    end
+    return true if visited[index]
 
+    # set current node to visited and stack
+    visited[index] = true
 
+    stack[index] = true
+
+    # create array with
 
 
   end
