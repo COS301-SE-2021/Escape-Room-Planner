41c7c74a2a7c2b1b6363c071f115a81bf89bdc9f
Patric Edwards <u18009337@tuks.co.za>
2021-08-08 18:16:57 +0200

    WoST

--- Backend/test/unit/solvabilityTests/SolvabilityTests.rb
+++ Backend/test/unit/solvabilityTests/SolvabilityTests.rb
@@ -4,11 +4,17 @@ require './app/Services/SolvabilitySubsystem/RequestSolvability/calculate_solvab
 require './app/Services/SolvabilitySubsystem/ResponseSolvability/calculate_solvability_response'
 
 class ErTest < ActiveSupport::TestCase
-  
+
   def test_solvability
-    
-    
+
+    vertices = [1,3,4,5,6]
+
+    solvabilityrq = CalculateSolvableRequest.new(1,6,vertices)
+
+    resp=calculate_solvability(solvabilityrq)
+
+
     assert_equal(true, true)
   end
-  
+
 end
