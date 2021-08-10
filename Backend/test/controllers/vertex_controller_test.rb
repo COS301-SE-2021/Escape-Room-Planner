43554598cdf365aeea974d1d1ea8ce946464c042
Kayla Latty <u17360812@tuks.co.za>
2021-08-10 15:08:06 +0200

    In vertex controller test. Added user auth to create key test

--- Backend/test/controllers/vertex_controller_test.rb
+++ Backend/test/controllers/vertex_controller_test.rb
@@ -62,14 +62,19 @@ class VertexControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'can create correct key' do
-    post api_v1_vertex_index_path, params: { type: 'Key',
-                                             name: 'key1',
-                                             posx: '2',
-                                             posy: '3',
-                                             width: '4',
-                                             height: '5',
-                                             graphicid: '123',
-                                             roomid: '1' }
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    post api_v1_vertex_index_path,
+         headers: { "Authorization": '"Bearer ' + res_l.token + '"' },
+         params: { type: 'Key',
+                   name: 'key1',
+                   posx: '2',
+                   posy: '3',
+                   width: '4',
+                   height: '5',
+                   graphicid: '123',
+                   roomid: '1' }
     assert_response :success
   end
 
