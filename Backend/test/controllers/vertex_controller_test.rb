17a9112179388a4214ce6fdf40565001004cb416
Kayla Latty <u17360812@tuks.co.za>
2021-08-10 15:14:25 +0200

    In vertex controller test. Added user auth to create incorrect container test

--- Backend/test/controllers/vertex_controller_test.rb
+++ Backend/test/controllers/vertex_controller_test.rb
@@ -112,13 +112,18 @@ class VertexControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'cant create Container' do
-    post api_v1_vertex_index_path, params: { type: 'Container',
-                                             posx: '2',
-                                             posy: '3',
-                                             width: '4',
-                                             height: '5',
-                                             graphicid: '123',
-                                             roomid: '1' }
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    post api_v1_vertex_index_path,
+         headers: { "Authorization": '"Bearer ' + res_l.token + '"' },
+         params: { type: 'Container',
+         posx: '2',
+         posy: '3',
+         width: '4',
+         height: '5',
+         graphicid: '123',
+         roomid: '1' }
     assert_response :bad_request
   end
 
