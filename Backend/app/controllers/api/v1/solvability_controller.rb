5374f404c88957fb4cee4437a1d4c753c9af9a40
Patric Edwards <u18009337@tuks.co.za>
2021-08-11 13:54:47 +0200

    Solvability controller

--- Backend/app/controllers/api/v1/solvability_controller.rb
+++ Backend/app/controllers/api/v1/solvability_controller.rb
@@ -1,4 +1,7 @@
 require './app/Services/services_helper'
+require './app/Services/SolvabilitySubsystem/SolvabilityServices'
+require './app/Services/SolvabilitySubsystem/RequestSolvability/calculate_solvability_request'
+require './app/Services/SolvabilitySubsystem/ResponseSolvability/calculate_solvability_response'
 
 # rubocop:disable Metrics/ClassLength
 module Api
@@ -8,19 +11,44 @@ module Api
     class VertexController < ApplicationController
       protect_from_forgery with: :null_session
 
-      def index
+      def show
         if authorise(request)
-          startVert = params[:startVertex]
+          
+          type=params[:type]
 
-          endVert = params[:endVertex]
+          if(type=='Solvable')
+            start_vert = params[:startVertex]
 
-          vertices = params[:vertices]
+            end_vert = params[:endVertex]
+
+            vertices = params[:vertices]
+
+            Solvability(start_vert, end_vert, vertices)
+          end
 
         else
           render json: { status: 'FAILED', message: 'Unauthorized' }, status: 401
         end
+      rescue StandardError
+        render json: { status: 'FAILED', message: 'Unspecified error' }, status: :bad_request 
+    end
+      
+      def Solvability(start_vert, end_vert, vertices)
+
+        if start_vert.nil? || end_vert.nil? || vertices.nil?
+          render json: { status: 'FAILED', message: 'Ensure correct parameters are given' }, status: :bad_request
+          return
+        end
+
+        req = CalculateSolvableRequest(start_vert, end_vert, vertices)
+        serv = SolvabilityService.new
+        resp = serv.calculate_solvability(req)
+
+        render json: { status: 'Response received', message: 'Data:', data: resp }, status: :ok
+        @vertices = vertices
       end
+    end
 
     end
   end
-end
+
--- Backend/test/unit/solvability_test.rb
+++ Backend/test/unit/solvability_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 require './app/Services/SolvabilitySubsystem/SolvabilityServices'
-require './app/Services/SolvabilitySubsystem/RequestSolvability/calculate_solvabily_request'
+require './app/Services/SolvabilitySubsystem/RequestSolvability/calculate_solvability_request'
 require './app/Services/SolvabilitySubsystem/ResponseSolvability/calculate_solvability_response'
 
 class SolvabilityTest < ActiveSupport::TestCase
