6e16466081fad9460c72de5e8ee3c624608544a3
Patric Edwards <u18009337@tuks.co.za>
2021-08-11 13:38:18 +0200

    Working on Solvability controller

--- Backend/app/controllers/api/v1/solvability_controller.rb
+++ Backend/app/controllers/api/v1/solvability_controller.rb
@@ -1,1 +1,12 @@
+# rubocop:disable Metrics/ClassLength
+module Api
+  # v1 model definition for api calls
+  module V1
+    # Controller that maps http requests to functions to execute
+    class VertexController < ApplicationController
+      protect_from_forgery with: :null_session
 
+
+    end
+  end
+end
