085b8d5d9b01a940455265d0cdf67c81cac8ca23
Donavan Roux <donavanroux2019@gmail.com>
2021-08-11 15:05:29 +0200

    updated schema.rb for new blobs and active storage

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_123526) do
+ActiveRecord::Schema.define(version: 2021_08_10_145438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "escape_rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -55,9 +83,12 @@ ActiveRecord::Schema.define(version: 2021_08_09_123526) do
     t.string "description"
     t.string "clue"
     t.bigint "escape_room_id", default: 1, null: false
+    t.bigint "blob_id"
     t.index ["escape_room_id"], name: "index_vertices_on_escape_room_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "escape_rooms", "users"
   add_foreign_key "vertices", "escape_rooms"
 end
