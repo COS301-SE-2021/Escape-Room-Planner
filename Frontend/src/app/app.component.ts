36ac96907b4eea1909f1fa0d63e4b22d165061d0
Egor <egorkharatyan@gmail.com>
2021-08-11 18:42:21 +0200

    Navbar, switches the lists if logged in/out (kinda Sadge) #2

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
+
+// TODO: just render the list at this point, idk how else to fix this, need to time reload to happen exactly once after login, at least quit is working
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,26 @@ import {Router} from "@angular/router";
 })
 export class AppComponent {
   private headers: HttpHeaders = new HttpHeaders();
-  private  is_valid_user = false;
+  private is_valid_user = false;
 
-  constructor(private httpClient: HttpClient, private router: Router) {
+  //todo
+
+  constructor(private httpClient: HttpClient, private router: Router, private cdr: ChangeDetectorRef) {
 
   }
 
   ngOnInit(){
     this.verifyJWT();
+
+    this.router.events.subscribe(event =>{
+      if (event instanceof NavigationEnd){
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  isUserValid(): boolean{
+    return this.is_valid_user;
   }
 
   verifyJWT(): void{
@@ -28,6 +42,7 @@ export class AppComponent {
 
     this.httpClient.post<any>('http://127.0.0.1:3000/api/v1/user', request_body, {"headers": this.headers}).subscribe(
       response => {
+        this.cdr.detectChanges();
         // response so go to escape room planner
         this.is_valid_user = true;
         this.router.navigate(['escape-room']).then(r => console.log('already logged in'));
@@ -42,13 +57,10 @@ export class AppComponent {
     );
   }
 
-  isUserValid(): boolean{
-    return this.is_valid_user;
-  }
-
   quit(): void{
     this.is_valid_user = false;
     localStorage.clear();
+    location.reload();
     this.router.navigate(['login']).then(r => console.log('user quit'));
   }
 
