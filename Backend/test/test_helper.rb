f7ca263b5d8b4bf633d45f310eaaad2ad3786c0a
Egor <egorkharatyan@gmail.com>
2021-08-11 11:30:04 +0200

    test_helper.rb has functions for requests.

--- Backend/test/test_helper.rb
+++ Backend/test/test_helper.rb
@@ -11,7 +11,45 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def authed_get_call(link)
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    get link,
+        headers: { "Authorization": "\"Bearer #{res_l.token}\"" }
 
+    # @response
+  end
 
+  def authed_post_call(link, in_params)
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    post link,
+         headers: { "Authorization": "\"Bearer #{res_l.token}\"" },
+         params: in_params
+  end
+
+  def authed_delete_call(link, in_params)
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    delete link,
+           headers: { "Authorization": "\"Bearer #{res_l.token}\"" },
+           params: in_params
+
+    JSON.parse(@response.body)
+  end
+
+  def authed_put_request(link, in_params)
+    us = UserServices.new
+    req_l = LoginRequest.new('testUser', 'testPass')
+    res_l = us.login(req_l)
+    put link,
+        headers: { "Authorization": "\"Bearer #{res_l.token}\"" },
+        params: in_params, as: :as_json
+
+    JSON.parse(@response.body)
+  end
   # Add more helper methods to be used by all tests here...
 end
