79f4dba31187a49ce68cc75e2ad10cc857022422
NQ-edile <nqfreelance@gmail.com>
2021-08-11 23:04:03 +0200

    Added routing for both login and signUp

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit() {
   }
@@ -26,7 +27,7 @@ export class LoginComponent implements OnInit {
       .subscribe(
         res => {
             localStorage.setItem('token: ', res["auth_token"]);
-            alert("Success");
+            this.router.navigate(['/']).then(r => alert("Success"));
         },
           error => {
             this.display = 'block';
--- Frontend/src/app/pages/signup/signup.component.ts
+++ Frontend/src/app/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Renderer2, ViewChild} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -8,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private router:Router) { }
   display = ' none'
   ngOnInit(): void {}
 
@@ -25,8 +26,7 @@ export class SignupComponent implements OnInit {
 
     this.http.post<any>(' http://127.0.0.1:3000/api/v1/user', extra_data)
       .subscribe((response)=> {
-        // response.token
-        alert('Success');
+          this.router.navigate(['/login']).then(r => alert("Success"));
       },
           error => {
             this.display = 'block';
