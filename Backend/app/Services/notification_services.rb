75d77da066d4205328a13447c10bf108283539d5
Kayla Latty <u17360812@tuks.co.za>
2021-08-12 15:27:28 +0200

    updated send_email notification service to use mail gem

--- Backend/app/Services/notification_services.rb
+++ Backend/app/Services/notification_services.rb
@@ -1,3 +1,19 @@
-def send_email_notification(request)
-  SendEmailNotificationResponse(false, 'SendEmailNotificationRequest null') if request.nil?
+require 'mail'
+
+def NotificationServices
+  def send_email_notification(request)
+    return SendEmailNotificationResponse(false, 'SendEmailNotificationRequest null') if request.nil?
+    @mail = Mail.new do
+      from 'alyak0803@gmail.com'
+      to   'kayla.latty.kal@gmail.com'
+      subject 'test email'
+      body 'Hi this is a test'
+    end
+
+    @response = if @mail.deliver!
+                  SendEmailNotificationResponse(true, 'email sent succesfully')
+                else
+                  SendEmailNotificationResponse(false, 'email unsuccessful')
+                end
+  end
 end
