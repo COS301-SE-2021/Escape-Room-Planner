937d4ce7d02355f75cfa967b041be02896e7dea7
Egor Kharatyan <egorkharatyan@gmail.com>
2021-08-12 20:53:13 +0200

    Delete test directory

--- test/controllers/vertex_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-class VertexControllerTest < ActionDispatch::IntegrationTest
-
-  test 'can get index' do
-    get api_v1_vertex_index_path
-    assert_response :success
-  end
-
-end
