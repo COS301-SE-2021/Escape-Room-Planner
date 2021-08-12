cf0947e45b051180dcf1093a0cf5497a5d843928
Egor Kharatyan <egorkharatyan@gmail.com>
2021-08-12 20:52:05 +0200

    Delete db directory

--- db/migrate/20210601132244_add_estimatedtime_to_vertices.rb
+++
@@ -1,7 +0,0 @@
-class AddEstimatedtimeToVertices < ActiveRecord::Migration[6.1]
-  def change
-    add_column :vertices, :estimatedTime, :time
-    add_column :vertices, :description, :string
-    add_column :vertices, :clue, :string
-  end
-end
--- db/migrate/20210601163553_add_foreignkey_to_table_vertices.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignkeyToTableVertices < ActiveRecord::Migration[6.1]
-  def change
-    add_column :vertices, :escape_rooms_id, :string
-  end
-end
