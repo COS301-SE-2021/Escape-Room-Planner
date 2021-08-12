990f2602e6c4a6709abedfc31ad967d39640850a
Donavan Roux <donavanroux2019@gmail.com>
2021-08-12 12:35:51 +0200

    added controller for get graphic and test for call

--- Backend/app/controllers/api/v1/inventory_controller.rb
+++ Backend/app/controllers/api/v1/inventory_controller.rb
@@ -4,6 +4,8 @@ require './app/Services/services_helper'
 require './app/Services/InventorySubsystem/inventory_service'
 require './app/Services/InventorySubsystem/Request/add_graphic_request'
 require './app/Services/InventorySubsystem/Response/add_graphic_response'
+require './app/Services/InventorySubsystem/Request/get_graphics_request'
+require './app/Services/InventorySubsystem/Response/get_graphics_response'
 
 module Api
   module V1
@@ -11,6 +13,18 @@ module Api
     class InventoryController < ApplicationController
       protect_from_forgery with: :null_session
       @@inventory_service = InventoryService.new
+
+      # get api/v1/inventory
+      def index
+        if authorise(request)
+          auth_token = request.headers['Authorization'].split(' ').last
+          req = GetGraphicsRequest.new(auth_token)
+          res = @@inventory_service.get_graphics(req)
+          render json: { success: res.success, message: res.message, image: res.image }, status: :ok
+        else
+          render json: { success: false, message: 'Unauthorized' }, status: 401
+        end
+      end
 
       # POST api/v1/inventory/
       def create
--- Backend/test/controllers/inventory_controller_test.rb
+++ Backend/test/controllers/inventory_controller_test.rb
@@ -2,21 +2,35 @@
 
 require 'test_helper'
 
-class Api::V1::InventoryControllerTest < ActionDispatch::IntegrationTest
-  test 'can add graphic to Inventory' do
-    test_image = './storage/test/clue1.png'
-    file = Rack::Test::UploadedFile.new(test_image, 'image/png')
-    authed_post_call(api_v1_inventory_index_path, { image: file })
-    response = JSON.parse(@response.body)
-    assert_response :success
-    assert_equal 'Graphic been added', response['message']
-  end
+module Api
+  module V1
+    class InventoryControllerTest < ActionDispatch::IntegrationTest
+      test 'can add graphic to Inventory' do
+        test_image = './storage/test/clue1.png'
+        file = Rack::Test::UploadedFile.new(test_image, 'image/png')
+        authed_post_call(api_v1_inventory_index_path, { image: file })
+        response = JSON.parse(@response.body)
+        assert_response :success
+        assert_equal 'Graphic been added', response['message']
+      end
 
-  test 'correct response when no image added' do
-    authed_post_call(api_v1_inventory_index_path, { imaget: 'test' })
-    response = JSON.parse(@response.body)
-    assert_response :success
-    assert_equal 'Error has occurred', response['message']
-  end
+      test 'correct response when no image added' do
+        authed_post_call(api_v1_inventory_index_path, { image_fake: 'test' })
+        response = JSON.parse(@response.body)
+        assert_response :success
+        assert_equal 'Error has occurred', response['message']
+      end
 
+      # TODO: inventory test with fake JWT
+
+      test 'can get graphics for user' do
+        authed_get_call(api_v1_inventory_index_path)
+        response = JSON.parse(@response.body)
+        assert_response :success
+        assert_not_nil response['image']
+      end
+
+      # TODO: Assert get expected response for get graphics
+    end
+  end
 end
