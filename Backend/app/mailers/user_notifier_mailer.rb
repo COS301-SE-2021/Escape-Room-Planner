d0c1edb5fc52bfdfb6a42bc5017593d121190b89
Kayla Latty <u17360812@tuks.co.za>
2021-08-13 17:14:44 +0200

    Added user_notifier_mailer

---
+++ Backend/app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,10 @@
+class UserNotifierMailer < ApplicationMailer
+  default from: 'alyak0803@gmail.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(email)
+    mail(to: email,
+         subject: 'test',
+         body: 'HI')
+  end
+end
