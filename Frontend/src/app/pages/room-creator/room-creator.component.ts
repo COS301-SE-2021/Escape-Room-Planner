81ae25b3acb1b3cb89d5daa2999e2e5a5ed4fb98
Egor <egorkharatyan@gmail.com>
2021-08-14 14:23:14 +0200

    useless log

--- Frontend/src/app/pages/room-creator/room-creator.component.ts
+++ Frontend/src/app/pages/room-creator/room-creator.component.ts
@@ -177,7 +177,6 @@ export class RoomCreatorComponent implements OnInit, AfterViewInit {
     //http request to rails api
     this.httpClient.get<any>("http://127.0.0.1:3000/api/v1/vertex/" + this.currentRoomId, {"headers": this.headers}).subscribe(
       response => {
-        console.log(response);
         //render all the vertices
         for (let vertex_t of response.data){
           //spawn objects out;
