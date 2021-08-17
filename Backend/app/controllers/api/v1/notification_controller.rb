9c6b2593365fc9bff24d685b70e51b75c77879ad
Patric Edwards <u18009337@tuks.co.za>
2021-08-17 10:41:21 +0200

    added includes

--- Backend/app/controllers/api/v1/notification_controller.rb
+++ Backend/app/controllers/api/v1/notification_controller.rb
@@ -1,8 +1,16 @@
+require './app/Services/services_helper'
+require './app/Services/reset_password_notification_request'
+require './app/Services/reset_password_notification_response'
+require './app/Services/user_services'
+
 module Api
   module V1
     class NotificationController < ApplicationController
       protect_from_forgery with: :null_session
 
+      def create
+
+      end
 
     end
   end
