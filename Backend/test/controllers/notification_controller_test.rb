91957ac319cadabfd90d53a6ed52435403e74909
Patric Edwards <u18009337@tuks.co.za>
2021-08-17 12:50:42 +0200

    call reset password with no email

--- Backend/test/controllers/notification_controller_test.rb
+++ Backend/test/controllers/notification_controller_test.rb
@@ -3,11 +3,17 @@ require 'concurrent'
 
 class NotificationControllerTest < ActionDispatch::IntegrationTest
 
-  test"can call reset password" do
+  test "can call reset password" do
       response = authed_post_call(api_v1_notification_index_path, { operation: 'Reset Password',
                                                                     email: "Patrice02052@gmail.com"})
 
       assert_response :success
+  end
+
+  test "call reset password with no email" do
+    response = authed_post_call(api_v1_notification_index_path, { operation: 'Reset Password'})
+
+    assert_response :bad_request
   end
 
 end
