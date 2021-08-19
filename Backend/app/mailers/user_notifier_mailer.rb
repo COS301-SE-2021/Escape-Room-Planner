02af93fb5d2678cd5b5c94e2a363ed17eac0fe88
Kayla Latty <u17360812@tuks.co.za>
2021-08-19 23:07:39 +0200

    Added token with expiration to reset password link

--- Backend/app/mailers/user_notifier_mailer.rb
+++ Backend/app/mailers/user_notifier_mailer.rb
@@ -4,9 +4,13 @@ class UserNotifierMailer < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_reset_password_email(email)
       #sends reset password link to user's email
+
+      @user = User.find_by_email(email)
+      @token = JsonWebToken.encode(id: @user.id)
+      @url = "http://localhost:4200/reset?token=#{@token}"
       mail(to: email,
-           subject: 'Confirm Reset Password',
-           body: 'http://localhost:4200/reset')
+           subject: 'Confirm Reset Password')
+           # body: @url)
   end
 
   def send_verify_account_email(email)
