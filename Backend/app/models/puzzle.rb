5bc386fa7500ef377dc50aa5941e2043869b3d36
Egor <egorkharatyan@gmail.com>
2021-08-23 15:32:24 +0200

    backend z-index for puzzle fixed

--- Backend/app/models/puzzle.rb
+++ Backend/app/models/puzzle.rb
@@ -6,4 +6,5 @@
 # Puzzle model that inherits from Vertex model
 class Puzzle < Vertex
   validates :estimatedTime, :description, presence: true # just checks presence, since can be anything for user.
+  attribute :z_index, :float, default: 5
 end
