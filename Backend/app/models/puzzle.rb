15a1eeb0e39c5522c5f308f6a78bef9e79af64f9
Donavan Roux <donavanroux2019@gmail.com>
2021-08-23 13:32:20 +0200

    updated models to include z-index

--- Backend/app/models/puzzle.rb
+++ Backend/app/models/puzzle.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# validates :estimated_time
+# validates :description
+# validates :z_index default of 5
+# Puzzle model that inherits from Vertex model
 class Puzzle < Vertex
-  # validates :nextV
   validates :estimatedTime, :description, presence: true # just checks presence, since can be anything for user.
-  # validates :clue
-  # validates :escape_room_id
 end
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_113646) do
+ActiveRecord::Schema.define(version: 2021_08_23_110516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_113646) do
     t.string "clue"
     t.bigint "escape_room_id", default: 1, null: false
     t.bigint "blob_id"
+    t.float "z_index"
     t.index ["escape_room_id"], name: "index_vertices_on_escape_room_id"
   end
 
