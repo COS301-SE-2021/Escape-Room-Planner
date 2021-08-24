ca947d32bbebcd590ff40efa6d3d71b261c5f0b1
Patric Edwards <u18009337@tuks.co.za>
2021-08-24 11:27:11 +0200

    GA structure

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_113646) do
+ActiveRecord::Schema.define(version: 2021_08_24_092220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2021_08_12_113646) do
     t.string "name"
     t.bigint "user_id", default: 1, null: false
     t.index ["user_id"], name: "index_escape_rooms_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
