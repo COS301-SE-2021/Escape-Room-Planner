41cf6ca0ead6f6268b82394249dfb2af8186d7c3
Patric Edwards <u18009337@tuks.co.za>
2021-08-26 21:40:20 +0200

    Cannot find error

--- Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
+++ Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
@@ -15,30 +15,57 @@ class GeneticAlgorithmService
     puts "====================================GA Starts========================================="
     puts "======================================================================================"
 
-    #Manipulate this if you want to mess with the initial edges for accuracy
-    @max_edge_initial_factor=1
-    @min_edge_initial_factor=1
+    # Manipulate this if you want to mess with the initial edges for accuracy
+    @max_edge_initial_factor = 1
+    @min_edge_initial_factor = 1
 
     if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?
       GeneticAlgorithmResponse.new('False', 'Parameters required')
     end
 
-    #@initial_population = [][]
+    # Array of 2D arrays for initial population
+    @initial_population
     initial_population_creation(request.vertices)
 
   end
   
   def initial_population_creation(vertices)
     # Max edges = n(n-1)
-    max_edges = (vertices.count)*(vertices.count - 1)*@max_edge_initial_factor
+    max_edges = (vertices.count) * (vertices.count - 1) * @max_edge_initial_factor
     puts "Max edges: #{max_edges}"
 
     # Min edges = n-1
-    min_edges = vertices.count - 1*@min_edge_initial_factor
+    min_edges = vertices.count - 1 * @min_edge_initial_factor
     puts "Min edges: #{min_edges}"
+    
+    # Single Chromosome
+    chromosone = create_chromosone(rand(min_edges..max_edges), vertices)
 
   end
 
+  def create_chromosone(num_edges, vertices)
+    i_count = 1
+    chromosone=[]
+    # Create that number of edges for the chromosome
+    while i_count < num_edges
+      return_vertices(vertices)
+      chromosone[i_count][0] = @vertex1
+      chromosone[i_count][1] = @vertex2
+
+    end
+
+    nil
+  end
+
+  def return_vertices(vertices)
+    @vertex1 = vertices(rand(vertices.count))
+    @vertex2 = vertices(rand(vertices.count))
+
+    # Checks on vertices come here
+    return_vertices(vertices) if @vertex1 == @vertex2
+  end
+
+  
   def calculate_fitness; end
 
   def selection; end
@@ -46,5 +73,6 @@ class GeneticAlgorithmService
   def crossover; end
 
   def mutation; end
-  
+
+
 end
