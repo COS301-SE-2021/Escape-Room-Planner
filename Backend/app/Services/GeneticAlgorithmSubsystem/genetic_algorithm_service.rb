d176af31aca08d26c2d0777767f4e9c0799cb5cf
Patric Edwards <u18009337@tuks.co.za>
2021-08-26 21:18:03 +0200

    Working on initial pop

--- Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
+++ Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
@@ -10,22 +10,31 @@ require './app/Services/GeneticAlgorithmSubsystem/Response/genetic_algorithm_res
 class GeneticAlgorithmService
 
   def genetic_algorithm(request)
+    puts ""
+    puts "======================================================================================"
+    puts "====================================GA Starts========================================="
+    puts "======================================================================================"
+
+    #Manipulate this if you want to mess with the initial edges for accuracy
+    @max_edge_initial_factor=1
+    @min_edge_initial_factor=1
+
     if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?
       GeneticAlgorithmResponse.new('False', 'Parameters required')
     end
 
-    @initial_population = [][]
-    initial_population_creation(vertices)
+    #@initial_population = [][]
+    initial_population_creation(request.vertices)
 
   end
   
   def initial_population_creation(vertices)
     # Max edges = n(n-1)
-    max_edges = vertices.count(vertices.count - 1)
+    max_edges = (vertices.count)*(vertices.count - 1)*@max_edge_initial_factor
     puts "Max edges: #{max_edges}"
 
     # Min edges = n-1
-    min_edges = vertices.count - 1
+    min_edges = vertices.count - 1*@min_edge_initial_factor
     puts "Min edges: #{min_edges}"
 
   end
