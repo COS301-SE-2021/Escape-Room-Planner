e11d6820b875fa64d1bbc64e5fc34df5e25604a6
Patric Edwards <u18009337@tuks.co.za>
2021-08-26 20:56:04 +0200

    Can calculate min and max number of edges for initial pop

--- Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
+++ Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
@@ -2,17 +2,31 @@
 require './app/Services/GeneticAlgorithmSubsystem/Request/genetic_algorithm_request'
 require './app/Services/GeneticAlgorithmSubsystem/Response/genetic_algorithm_response'
 
+# Graph theory
+# Max number edges undirected graph n(n-1)/2
+# Max edges directed graph n(n-1)
+
+
 class GeneticAlgorithmService
 
   def genetic_algorithm(request)
     if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?
-        GeneticAlgorithmResponse.new('False','Parameters required')
+      GeneticAlgorithmResponse.new('False', 'Parameters required')
     end
 
+    @initial_population = [][]
+    initial_population_creation(vertices)
 
   end
   
   def initial_population_creation(vertices)
+    # Max edges = n(n-1)
+    max_edges = vertices.count(vertices.count-1)
+    puts "Max edges: #{max_edges}"
+
+    # Min edges = n-1
+    min_edges = vertices.count-1
+    puts "Min edges: #{min_edges}"
 
   end
 
