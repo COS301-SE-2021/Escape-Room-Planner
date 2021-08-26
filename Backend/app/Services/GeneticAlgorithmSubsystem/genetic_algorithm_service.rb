d19f665021bbe10487ed9b29b383892ea089ccc4
Patric Edwards <u18009337@tuks.co.za>
2021-08-26 20:26:03 +0200

    Pre flight checks

--- Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
+++ Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
+require './app/Services/GeneticAlgorithmSubsystem/Request/genetic_algorithm_request'
+require './app/Services/GeneticAlgorithmSubsystem/Response/genetic_algorithm_response'
 
 class GeneticAlgorithmService
 
   def genetic_algorithm(request)
-    if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?\
-
+    if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?
+        GeneticAlgorithmResponse.new("False","Parameters required")
     end
 
 
