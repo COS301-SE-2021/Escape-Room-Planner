c86e331d29f45d9207050f103bd195d778a6643b
Kayla Latty <u17360812@tuks.co.za>
2021-08-27 12:05:11 +0200

    Added verify-failed functionality

--- Frontend/src/app/pages/verify-failed/verify-failed.component.ts
+++ Frontend/src/app/pages/verify-failed/verify-failed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-verify-failed',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerifyFailedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(data:any) {
+
+
+    let extra_data = {
+      email: data["email"],
+      operation: 'Verify Account'
+    };
+
+    this.http.post<any>(' http://127.0.0.1:3000/api/v1/notification', extra_data)
+      .subscribe((response) => {
+          this.router.navigate(['/login']).then(r => alert("Success"));
+        },
+        error => {
+          alert("Error sending the email please try again")
+        })
   }
 
 }
