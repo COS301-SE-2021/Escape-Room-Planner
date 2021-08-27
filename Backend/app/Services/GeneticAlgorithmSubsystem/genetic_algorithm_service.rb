c0b1865e5d45923d05eb0e2196ed1f4098b3746b
Patric Edwards <u18009337@tuks.co.za>
2021-08-27 14:04:25 +0200

    Minimized duplication

--- Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
+++ Backend/app/Services/GeneticAlgorithmSubsystem/genetic_algorithm_service.rb
@@ -7,10 +7,7 @@ require './app/Services/GeneticAlgorithmSubsystem/Response/genetic_algorithm_res
 # Max edges directed graph n(n-1)
 
 class GeneticAlgorithmService
-  @clue_const = 'Clue'
-  @key_const = 'Keys'
-  @container_const = 'Container'
-  @puzzle_const = 'Puzzle'
+
   def genetic_algorithm(request)
     puts ""
     puts "======================================================================================"
@@ -18,7 +15,7 @@ class GeneticAlgorithmService
     puts "======================================================================================"
 
     # Manipulate this if you want to mess with the initial edges for accuracy
-    @max_edge_initial_factor = 0.5
+    @max_edge_initial_factor = 0.25
     @min_edge_initial_factor = 0.5
 
     if request.vertices.nil? || request.linear.nil? || request.dead_nodes.nil?
@@ -64,10 +61,10 @@ class GeneticAlgorithmService
 
     while i_count < num_edges
       @i_stop = 0
-       return_vertices(vertices)
+       return_vertices(vertices,i_count)
        @chromosome[i_count][0] = @vertex1
        @chromosome[i_count][1] = @vertex2
-       i_count += 1
+      i_count += 1
     end
 
     @initial_population.push(@chromosome)
@@ -75,57 +72,55 @@ class GeneticAlgorithmService
     puts "             ===================Chromosome number:" + (@chromosome_count + 1).to_s + "===================================="
     puts "             =========================================================================="
     i_count = 0
-    while i_count < num_edges - 1
-      puts Vertex.find_by_id(@chromosome[i_count][0]).type+":"+@chromosome[i_count][0].to_s + "," + Vertex.find_by_id(@chromosome[i_count][1]).type+":"+@chromosome[i_count][1].to_s
+    while i_count < num_edges
+      puts Vertex.find_by_id(@chromosome[i_count][0]).type + ":" + @chromosome[i_count][0].to_s + "," + Vertex.find_by_id(@chromosome[i_count][1]).type + ":" + @chromosome[i_count][1].to_s
       i_count += 1
     end
     @initial_population[@chromosome_count] = @chromosome
     @chromosome_count += 1
   end
 
-  def return_vertices(vert)
+  def return_vertices(vert,i_count)
     @vertex1 = vert[rand(vert.count)]
     @vertex2 = vert[rand(vert.count)]
-
     # Checks on vertices come here
-    return_vertices(vert) if @vertex1 == @vertex2
-
-    # This stops infinite recursion
+    return_vertices(vert,i_count) if @vertex1 == @vertex2
+    t1 = Vertex.find_by_id(@vertex1).type
+    t2 = Vertex.find_by_id(@vertex2).type
 
     # Check legal moves
-    if (Vertex.find_by_id(@vertex1).type == @clue_const || Vertex.find_by_id(@vertex1).type == @key_const) && !Vertex.find_by_id(@vertex2).type == @puzzle_const
-      if @i_stop < 4
+    if (t1 == 'Clue' || t1 == 'Keys') && t2 != 'Puzzle'
+      if @i_stop < 5
         @i_stop += 1
-        return_vertices(vert)
-      else
-        @vertex1 = nil
-        @vertex2 = nil
+        return_vertices(vert,i_count)
       end
     end
 
-    if Vertex.find_by_id(@vertex1).type == @container_const && Vertex.find_by_id(@vertex2).type == @puzzle_const
-      if @i_stop < 4
+    if t1 == 'Container' && t2 == 'Puzzle'
+      if @i_stop < 5
         @i_stop += 1
-        return_vertices(vert)
-      else
-        @vertex1 = nil
-        @vertex2 = nil
+        return_vertices(vert,i_count)
       end
     end
 
-    if Vertex.find_by_id(@vertex1).type == @puzzle_const && !Vertex.find_by_id(@vertex2).type == @container_const
-      if @i_stop < 4
+    if t1 == 'Puzzle' && t2 != 'Container'
+      if @i_stop < 5
         @i_stop += 1
-        return_vertices(vert)
-      else
-        @vertex1 = nil
-        @vertex2 = nil
+        return_vertices(vert,i_count)
+      end
+    end
+
+    if (i_count > 1) && (@chromosome[i_count - 1].include? @vertex1) && (@chromosome[i_count - 1].include? @vertex2)
+      if @i_stop < 5
+        @i_stop += 1
+        puts "here"
+        return_vertices(vert,i_count)
       end
     end
 
 
 
-  end
+    end
 
   
   def calculate_fitness; end
