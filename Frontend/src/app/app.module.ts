535320534f36e9f17a467783cc30dffcabc56283
NQ-edile <nqfreelance@gmail.com>
2021-08-28 13:59:38 +0200

    Implementing dependency diagram

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ResetPasswordComponent } from './pages/reset-password/reset-password.co
 import { ResetPasswordNotComponent } from './pages/reset-password-not/reset-password-not.component';
 import { InventoryComponent } from './pages/inventory/inventory.component';
 import { SolvabilityComponent } from './pages/solvability/solvability.component';
+import { DependencyDiagramComponent } from './pages/dependency-diagram/dependency-diagram.component';
 
 const routes: Routes = [
   // { path: '', component: AppComponent },
@@ -22,6 +23,8 @@ const routes: Routes = [
   { path: 'reset', component: ResetPasswordComponent },
   { path: 'reset-not', component: ResetPasswordNotComponent },
   { path: 'escape-room', component: RoomCreatorComponent},
+  { path: 'escape-room', component: RoomCreatorComponent},
+  { path: 'escape', component: DependencyDiagramComponent },
   // otherwise redirect to login
   { path: '**', redirectTo: '' }
 ];
@@ -36,7 +39,8 @@ const routes: Routes = [
     ResetPasswordComponent,
     ResetPasswordNotComponent,
     InventoryComponent,
-    SolvabilityComponent
+    SolvabilityComponent,
+    DependencyDiagramComponent
   ],
     imports: [
         BrowserModule,
--- Frontend/src/app/pages/dependency-diagram/dependency-diagram.component.ts
+++ Frontend/src/app/pages/dependency-diagram/dependency-diagram.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as go from 'gojs';
+import { VertexService } from 'src/app/services/vertex.service';
 
 @Component({
   selector: 'app-dependency-diagram',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DependencyDiagramComponent implements OnInit {
 
-  constructor() { }
+  constructor(vertexService: VertexService) { }
 
   ngOnInit(): void {
   }
+
+
 
 }
