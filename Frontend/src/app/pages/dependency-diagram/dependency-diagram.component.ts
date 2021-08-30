3b65dbc634bd934aab840a8ce888c8cf3ee1b022
NQ-edile <nqfreelance@gmail.com>
2021-08-30 22:03:00 +0200

    Added a few loops to try and generate the diagram

--- Frontend/src/app/pages/dependency-diagram/dependency-diagram.component.ts
+++ Frontend/src/app/pages/dependency-diagram/dependency-diagram.component.ts
@@ -29,21 +29,21 @@ export class DependencyDiagramComponent implements OnInit, AfterViewInit {
       graph.getModel().beginUpdate();
 
       for (let vertex of this.vertexService.vertices){
-        const vert1 = graph.insertVertex(parent, vertex.id, vertex.name, 0, 0, 80, 80);
-        const vert2 = graph.insertVertex(parent, )
+        const vert1 = graph.insertVertex(parent, vertex.id, vertex.type, 0, 0, 80, 80);
+        for (let edge of vert1.getConnections())
+          graph.insertVertex(parent, '', '', edge, vert1);
       }
 
-
-      const vertex1 = graph.insertVertex(parent, '1', 'Start', 0, 0, 80, 80);
-      const vertex2 = graph.insertVertex(parent, '2', 'Container', 0, 0, 80, 80);
-      const vertex3 = graph.insertVertex(parent, '3', 'Puzzle', 0, 0, 80, 80);
-      const vertex4 = graph.insertVertex(parent, '4', 'Clue', 0, 0, 80, 80);
-      const vertex5 = graph.insertVertex(parent, '5', 'End', 0, 0, 80, 80);
-      graph.insertEdge(parent, '', '', vertex1, vertex2);
-      graph.insertEdge(parent, '', '', vertex1, vertex4);
-      graph.insertEdge(parent, '', '', vertex2, vertex3);
-      graph.insertEdge(parent, '', '', vertex4, vertex5);
-      graph.insertEdge(parent, '', '', vertex3, vertex5);
+      // const vertex1 = graph.insertVertex(parent, '1', 'Start', 0, 0, 80, 80);
+      // const vertex2 = graph.insertVertex(parent, '2', 'Container', 0, 0, 80, 80);
+      // const vertex3 = graph.insertVertex(parent, '3', 'Puzzle', 0, 0, 80, 80);
+      // const vertex4 = graph.insertVertex(parent, '4', 'Clue', 0, 0, 80, 80);
+      // const vertex5 = graph.insertVertex(parent, '5', 'End', 0, 0, 80, 80);
+      // graph.insertEdge(parent, '', '', vertex1, vertex2);
+      // graph.insertEdge(parent, '', '', vertex1, vertex4);
+      // graph.insertEdge(parent, '', '', vertex2, vertex3);
+      // graph.insertEdge(parent, '', '', vertex4, vertex5);
+      // graph.insertEdge(parent, '', '', vertex3, vertex5);
     } finally {
       graph.getModel().endUpdate();
       new mxHierarchicalLayout(graph).execute(graph.getDefaultParent());
