016cd7a5560f3d887020b18be09e247416d3759c
Egor <egorkharatyan@gmail.com>
2021-09-06 14:18:59 +0200

    prod mode works fine now?

--- Frontend/src/app/pages/room-creator/room-creator.component.ts
+++ Frontend/src/app/pages/room-creator/room-creator.component.ts
@@ -58,7 +58,6 @@ export class RoomCreatorComponent implements OnInit, AfterViewInit {
   constructor(private el : ElementRef, private renderer: Renderer2, private httpClient: HttpClient,
               private vertexService: VertexService, private router:Router)
   {
-
     if(localStorage.getItem('token') ==  null) {
       this.router.navigate(['login']).then(r => console.log('no jwt stored'));
     }
