0c8127adf54a5e9ed0a79c1b0f3c70d916d79801
Donavan Roux <donavanroux2019@gmail.com>
2021-09-15 17:48:45 +0200

    added more styling to solvability component and fixed layout

--- Frontend/src/app/pages/solvability/solvability.component.ts
+++ Frontend/src/app/pages/solvability/solvability.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {DependencyDiagramComponent} from "../dependency-diagram/dependency-diagram.component";
+import {VertexService} from "../../services/vertex.service";
 
 @Component({
   selector: 'app-solvability',
@@ -20,53 +21,43 @@ export class SolvabilityComponent implements OnInit {
   @ViewChild("solve_div") solve_div: ElementRef | undefined;
   @ViewChild(DependencyDiagramComponent) diagramComponent: DependencyDiagramComponent | undefined;
 
-  constructor(private el : ElementRef, private httpClient: HttpClient) {
+  public error_message = "";
+  public start_vertex_name = "none";
+  public end_vertex_name = "none";
+  public estimated_time = "none";
+
+  constructor(private el: ElementRef, private httpClient: HttpClient, private vertexService: VertexService) {
     this.headers = this.headers.set('Authorization1', 'Bearer ' + localStorage.getItem('token'))
-      .set("Authorization2",'Basic ' + localStorage.getItem('username'));
+      .set("Authorization2", 'Basic ' + localStorage.getItem('username'));
   }
 
   ngOnInit(): void {
     this._current_room_id = -1;
   }
 
-  setRoom(room_id: number){
+  setRoom(room_id: number) {
     this._current_room_id = room_id;
   }
 
-  getInitialVertices():void{
-    if(this._current_room_id !== -1) {
-      this.httpClient.get<any>(environment.api + "/api/v1/room/" + this._current_room_id, {"headers": this.headers}).subscribe(
-        response => {
+  getInitialVertices(): void {
+    if (this._current_room_id !== -1) {
+      let start_vertex = this.vertexService.vertices[this.vertexService.start_vertex_id];
+      let end_vertex = this.vertexService.vertices[this.vertexService.end_vertex_id];
 
-          // @ts-ignore
-          document.getElementById("Start-Vertex-label").innerHTML = "Start Vertex: " + response.data.startVertex;
-          this._target_start = response.data.startVertex;
-
-          // @ts-ignore
-          document.getElementById("End-Vertex-label").innerHTML = "End Vertex: " + response.data.endVertex;
-          this._target_end = response.data.endVertex
-        },
-        //Render error if bad request
-        error => alert('There was an error retrieving the start vertices')
-      );
+      this._target_start = start_vertex.id;
+      this.start_vertex_name = start_vertex.name;
+      this._target_end = end_vertex.id;
+      this.end_vertex_name = end_vertex.name;
     }
   }
 
-  checkSolvable(target_start: HTMLElement, target_end: HTMLElement, current_room_id: Number){
+  checkSolvable(target_start: HTMLElement, target_end: HTMLElement, current_room_id: Number) {
+    this.getInitialVertices();
     this._target_start = target_start;
     this._target_end = target_end;
     this._current_room_id = current_room_id
-    if(this._target_start==null){
-      this.setSolvability(false);
-      window.alert('set a start vertex first');
-      return;
-    }
+    this.checkStartEndVertex();
 
-    if(this._target_end==null){
-      this.setSolvability(false);
-      window.alert('set an end vertex first');
-      return;
-    }
 
     let SolvableCheck = {
       operation: "Solvable",
@@ -82,205 +73,121 @@ export class SolvabilityComponent implements OnInit {
       response => {
         //rendering <li> elements by using render function
         this.setSolvability(response.data.solvable);
-
-        if(response.data.solvable==false){
-         if(response.data.reason === "No reason given")
-           window.alert('Could not find connection between objects')
+        this.error_message = "";
+        if (response.data.solvable == false) {
+          if (response.data.reason === "No reason given")
+            this.error_message = "Could not find connection between objects";
           else
-          window.alert(response.data.reason)
+            this.error_message = response.data.reason;
         }
       },
       error => console.error('', error)
     );
   }
 
-  setSolvability(input: any): void{
+  setSolvability(input: any): void {
     this.checkPaths()
     this.checkUnnecessary()
     this.checkEstimatedTime()
-    if(input){
+    if (input) {
       // @ts-ignore
       this.solve_div?.nativeElement.setAttribute('class', 'modal-body rounded border border-4 border-success bg-dark');
       // @ts-ignore
-      document.getElementById("Start-Vertex-label").innerText="Start Vertex: "+this._target_start
-      // @ts-ignore
-      document.getElementById("End-Vertex-label").innerText="End Vertex: "+this._target_end
-      // @ts-ignore
-      document.getElementById("Solvable").innerHTML="Solvable: True"
-    }else{
+      document.getElementById("Solvable").innerHTML = "Solvable: True"
+    } else {
       // @ts-ignore
       this.solve_div?.nativeElement.setAttribute('class', 'modal-body rounded border border-4 border-danger bg-dark');
       // @ts-ignore
-      document.getElementById("Solvable").innerHTML="Solvable: False"
-      // @ts-ignore
-      document.getElementById("Start-Vertex-label").innerText="Start Vertex: "+this._target_start
-      // @ts-ignore
-      document.getElementById("End-Vertex-label").innerText="End Vertex: "+this._target_end
+      document.getElementById("Solvable").innerHTML = "Solvable: False"
     }
   }
 
-  display(Paramaters : any, reason :any){
-    let final=''
-    this.httpClient.post<any>(environment.api + "/api/v1/solvability/", Paramaters, {"headers": this.headers}).subscribe(
+  display(Parameters: any, reason: any) {
+    let final = ''
+    this.httpClient.post<any>(environment.api + "/api/v1/solvability/", Parameters, {"headers": this.headers}).subscribe(
       response => {
         //rendering <li> elements by using render function
-        if (reason=="Paths"){
-        response.data.vertices.forEach(
-          (value: any) => {
-            if (reason=="Paths"){
-              // @ts-ignore
-             final=final+value+"<br>"
+        if (reason == "Paths") {
+          let i = 1;
+          response.data.vertices.forEach(
+            (value: any) => {
+              if (reason == "Paths") {
+                let int_array = this.vertexService.convertToLocalID(value.split(","));
+                final += i + ": ";
+                for (let vertex_id of int_array) {
+                  final += this.vertexService.vertices[vertex_id].name + ", ";
+                }
+                // @ts-ignore
+                final += "<br>";
+                i++;
+              }
             }
-          }
-        )
+          )
           // @ts-ignore
-          document.getElementById("PossbilePaths").innerHTML="Possible Paths: <br>"+ final;
+          document.getElementById("possible_paths").innerHTML = "Possible Paths: <br>" + final;
         }
 
-        if(reason=="Time"){
+        if (reason == "Time") {
           // @ts-ignore
-          document.getElementById("EstimatedTime").innerHTML="Estimated Time: <br>"+ (response.data.time/60);
+          this.estimated_time = Math.round(response.data.time / 60) + " min";
         }
       },
       error => console.error('', error)
     );
   }
 
-  checkUnnecessary(){
-    if(this._target_start==null){
-      this.setSolvability(false);
-      window.alert('set a start vertex first');
-      return;
+  checkUnnecessary() {
+    if (this.checkStartEndVertex()) {
+
+      let FindUnnecessary = {
+        operation: "FindUnnecessary",
+        roomid: this._current_room_id
+      };
+
+      this.display(FindUnnecessary, "Unnecessary")
     }
-
-    if(this._target_end==null){
-      this.setSolvability(false);
-      window.alert('set an end vertex first');
-      return;
-    }
-
-    let FindUnnecessary = {
-      operation: "FindUnnecessary",
-      roomid: this._current_room_id
-    };
-
-    this.display(FindUnnecessary,"Unnecessary")
-
   }
 
-  checkEstimatedTime(){
-    if(this._target_start==null){
-      this.setSolvability(false);
-      window.alert('set a start vertex first');
-      return;
+  checkEstimatedTime() {
+    if (this.checkStartEndVertex()) {
+      let PathsCheck = {
+        operation: "EstimatedTime",
+        roomid: this._current_room_id
+      };
+      this.display(PathsCheck, "Time")
     }
-
-    if(this._target_end==null){
-      this.setSolvability(false);
-      window.alert('set an end vertex first');
-      return;
-    }
-
-    let PathsCheck = {
-      operation: "EstimatedTime",
-      roomid: this._current_room_id
-    };
-
-    this.display(PathsCheck,"Time")
   }
 
-  checkPaths(){
-    if(this._target_start==null){
-      this.setSolvability(false);
-      window.alert('set a start vertex first');
-      return;
+  checkPaths() {
+    if (this.checkStartEndVertex()) {
+
+      let PathsCheck = {
+        operation: "ReturnPaths",
+        roomid: this._current_room_id
+      };
+
+      this.display(PathsCheck, "Paths")
     }
-
-    if(this._target_end==null){
-      this.setSolvability(false);
-      window.alert('set an end vertex first');
-      return;
-    }
-
-    let PathsCheck = {
-      operation: "ReturnPaths",
-      roomid: this._current_room_id
-    };
-
-    this.display(PathsCheck,"Paths")
-    // document.getElementById("SetupOrder").innerHTML="Set up order: "+resp.data.order;
-
-  }
-
-  checkSetupOrder() {
-    if(this._target_start==null){
-      window.alert('set a start vertex first')
-      return
-    }
-
-    if(this._target_end==null){
-      window.alert('set an end vertex first')
-      return
-    }
-
-    let SolvableCheck = {
-      operation: "Solvable",
-      startVertex: this._target_start,
-      endVertex: this._target_end,
-      roomid: this._current_room_id
-    };
-
-    this.httpClient.post<any>(environment.api + "/api/v1/solvability/", SolvableCheck, {"headers": this.headers}).subscribe(
-      response => {
-        //rendering <li> elements by using render function
-        console.log(response)
-        if (response.data.solvable==true){
-        }else {
-          this.setSolvability(false);
-        }
-
-
-      },
-      error => console.error('', error)
-    );
-
-    let setUpOrderCheck = {
-      operation: "Setup",
-      startVertex: this._target_start,
-      endVertex: this._target_end,
-      roomid: this._current_room_id
-    };
-
-    this.httpClient.post<any>(environment.api + "/api/v1/solvability/", setUpOrderCheck, {"headers": this.headers}).subscribe(
-      resp => {
-        console.log(resp)
-      //  let order =[]
-      //  let i=0
-
-
-        if(resp.data.status=="Success"){
-          // @ts-ignore
-          document.getElementById("SetupOrder").innerHTML="Set up order: "+resp.data.order;
-          /* resp.data.order.forEach(
-             (value: any) => {
-              this.httpClient.get<any>(environment.api + "/api/v1/vertex/"+value).subscribe(
-                 resp => {
-                   console.log(resp)
-                 }
-             )
-             } )*/
-        }else {
-          window.alert('Unknown failure')
-        }
-      },
-      error => console.error('', error)
-    );
-
-
   }
 
   generateDiagram(): void {
     this.diagramComponent?.generate();
+  }
+
+  checkStartEndVertex(): boolean {
+    this.error_message = "";
+    if (this._target_start == null) {
+      this.setSolvability(false);
+      this.error_message = "set a start vertex first";
+      return false;
+    }
+
+    if (this._target_end == null) {
+      this.setSolvability(false);
+      this.error_message = "set an end vertex first";
+      return false;
+    }
+    return true;
   }
 
 }
