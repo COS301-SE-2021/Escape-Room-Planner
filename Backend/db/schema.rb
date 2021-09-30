fdba0bd5dcdc568d0e95428c9712cbfc75d4119f
Patric Edwards <u18009337@tuks.co.za>
2021-09-30 09:27:01 +0200

    Rating model added

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_064122) do
+ActiveRecord::Schema.define(version: 2021_09_30_072432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 2021_09_30_064122) do
     t.bigint "blob_id"
     t.bigint "escape_room_id", null: false
     t.index ["escape_room_id"], name: "index_room_images_on_escape_room_id"
+  end
+
+  create_table "room_ratings", force: :cascade do |t|
+    t.integer "RoomID"
+    t.integer "Rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "rooms", force: :cascade do |t|
