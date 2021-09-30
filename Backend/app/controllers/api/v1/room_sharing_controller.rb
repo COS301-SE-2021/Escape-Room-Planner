4a06f8a7d64fd6dbf049ecfcb121ccac83b05f6e
Patric Edwards <u18009337@tuks.co.za>
2021-09-30 08:37:24 +0200

    RoomSharingController

---
+++ Backend/app/controllers/api/v1/room_sharing_controller.rb
@@ -0,0 +1,15 @@
+require './app/Services/room_services'
+require './app/Services/services_helper'
+require './app/Services/create_escaperoom_request'
+require './app/Services/create_escaperoom_response'
+require './app/Services/RoomSubsystem/Request/get_rooms_request'
+require './app/Services/RoomSubsystem/Response/get_rooms_response'
+
+module Api
+  module V1
+    class RoomSharingController < ApplicationController
+
+    end
+  end
+end
+
