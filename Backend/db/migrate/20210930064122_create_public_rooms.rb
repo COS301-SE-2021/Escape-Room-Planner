01139cbba128255e524906e5789fae52dd41666e
Egor Kharatyan <egorkharatyan@gmail.com>
2021-10-01 17:15:47 +0200

    Revert "In theory controllers work"

--- Backend/db/migrate/20210930064122_create_public_rooms.rb
+++
@@ -1,9 +0,0 @@
-class CreatePublicRooms < ActiveRecord::Migration[6.1]
-  def change
-    create_table :public_rooms do |t|
-      t.integer :RoomID
-
-      t.timestamps
-    end
-  end
-end
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_072432) do
+ActiveRecord::Schema.define(version: 2021_09_17_193650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,12 +60,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_072432) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "public_rooms", force: :cascade do |t|
-    t.integer "RoomID"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "room_images", force: :cascade do |t|
     t.float "pos_x"
     t.float "pos_y"
@@ -74,18 +68,6 @@ ActiveRecord::Schema.define(version: 2021_09_30_072432) do
     t.bigint "blob_id"
     t.bigint "escape_room_id", null: false
     t.index ["escape_room_id"], name: "index_room_images_on_escape_room_id"
-  end
-
-  create_table "room_ratings", force: :cascade do |t|
-    t.integer "RoomID"
-    t.integer "Rating"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "rooms", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
