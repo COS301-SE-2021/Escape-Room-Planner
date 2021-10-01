df75e3c6965a9fec11f889c4b92180a64c5b876b
Patric Edwards <u18009337@tuks.co.za>
2021-10-01 09:00:51 +0200

    working on index

--- Backend/app/controllers/api/v1/room_sharing_controller.rb
+++ Backend/app/controllers/api/v1/room_sharing_controller.rb
@@ -4,20 +4,33 @@ require './app/Services/create_escaperoom_request'
 require './app/Services/create_escaperoom_response'
 require './app/Services/RoomSubsystem/Request/get_rooms_request'
 require './app/Services/RoomSubsystem/Response/get_rooms_response'
+require './app/Services/SolvabilitySubsystem/RequestSolvability/calculate_estimated_time_request'
+require './app/Services/SolvabilitySubsystem/ResponseSolvability/calculate_estimated_time_response'
+require './app/Services/SolvabilitySubsystem/SolvabilityServices'
 
 module Api
   module V1
     class RoomSharingController < ApplicationController
-      #Get vertices
+      # Get vertices
       def index
-        #Room rating username time
-        rooms = PublicRoom.all
+        # Room rating username time
+
 
         return_array = Array.new( rooms.size) { Array.new(4) }
-        return_array[0] = rooms
+        
+        # Populate rooms
+        i_count = 0
+        rooms = PublicRoom.all
+        rooms.each do |room|
+          return_array[i_count][0] = room
+          return_array[i_count][1] = 3.5
 
-        #Default rating for now
-    
+          escape_room=EscapeRoom.find_by(room.RoomID)
+          return_array[i_count][2] = escapeRoom.
+
+          i_count += 1
+        end
+
 
         render json: { status: 'success', data: return_array }, status: :ok
       end
